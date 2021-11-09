34033ca4b56706ca83b2252f4439c42115640ae6
M_OlivaG <marco.oliva@upc.edu>
2021-11-09 18:18:31 +0100

    PARAM: Se cambio el nombre de las variables como en el codigo de Dcr2D. Se agrego el parametro tensor.dat. Se cambio el nombre de elements a lnods y de nodes a coord, asi como en los archivos de entrada

--- mod_param.f90
+++ mod_param.f90
@@ -3,25 +3,27 @@ module param
   implicit none
   
   character(len=14), parameter  :: ElemType = 'Quadrilateral'
-  integer, parameter :: DimPr     = 2     !Dimension del problema 
-  integer, parameter :: Nelem     = 100  !Number of elements
-  integer, parameter :: n_nodes   = 121 !Total number of velocity nodes
-  integer, parameter :: n_pnodes  = 121  !Total number of preasure nodes MAXVAL(pnodes,2)
-  integer, parameter :: nUne      = 4     !Number of velocity nodes in the element
-  !integer, parameter :: nPne      = 4     !Number of preasure nodes in the element
-  integer, parameter :: Dof       = 3     !Degrees of fredoom: 2 of velocity + 1 of preasure
-  integer, parameter :: totGp     = 4     ! 1,4,9 for Q, 1,3,4,6 for P 
-  real(4), parameter :: materials = 1.0 
+  integer, parameter :: DimPr    = 2    !Dimension del problema 
+  integer, parameter :: nelem    = 100  !Number of lnods
+  integer, parameter :: nnodes   = 121  !Total number of nodal points
+  integer, parameter :: nne      = 4    !Number of coord in the element
+  integer, parameter :: ndofn    = 3    !Degrees of freedom
+  integer, parameter :: totGp    = 4    ! 1,4,9 for Q, 1,3,4,6 for P 
 
 
-  character(len=20), parameter :: File_element  = 'elements.dat'
-  character(len=20), parameter :: File_nodes    = 'nodes.dat'
+  character(len=20), parameter :: File_element  = 'lnods.dat'
+  character(len=20), parameter :: File_coord    = 'coord.dat'
   character(len=29), parameter :: File_PostMsh  = 'CDR3d.post.msh'
   character(len=29), parameter :: File_PostRes  = 'CDR3d.post.res'
+  character(len=20), parameter :: File_tensors  = 'tensors.dat'
 
-  double precision, allocatable, dimension(:,:) :: gauss_points, gauss_weights !Verificar si debe ser global
-  integer, dimension(Nelem, nUne + 1)    :: elements
-  real,    dimension(n_nodes, DimPr + 1) :: nodes
+  double precision, allocatable, dimension(:,:) :: ngaus, weigp !Verificar si debe ser global
+  real, dimension(nnodes, DimPr+1)              :: coord
+  integer, dimension(nelem, nne + 1)            :: lnods
+
+
+
+
 
 end module param
 
