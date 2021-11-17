41af735f3dcf7718d8a61aa684bf7f3175a26e36
M_OlivaG <marco.oliva@upc.edu>
2021-11-17 20:42:50 +0100

    PARAM: Se agregaron parametros kstab,patau,ktaum

--- mod_param.f90
+++ mod_param.f90
@@ -11,7 +11,13 @@ module param
   integer, parameter :: totGp    = 4                        !1,4,9 for Q, 1,3,4,6 for P 
   integer, parameter :: nevab    = ndofn*nne                !Number of element variables
   integer, parameter :: ntotv    = ndofn*nnodes             !Total number of variables
+  integer, parameter :: kstab    = 3                        !Type of stabilization 
+  integer, parameter :: ktaum    = 0                        !Type of tau matrix
   real*8,  parameter :: hnatu    = 2.0                      !Reference element length    
+  real*8,  parameter :: patau    = 1.0                      !Parameter to obtain tau
+  
+
+
 
   character(len=20), parameter :: File_element  = 'lnods.dat'
   character(len=20), parameter :: File_coord    = 'coord.dat'
