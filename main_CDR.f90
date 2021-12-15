83e384119005917d0706e48abff02ce25a14417c
M_OlivaG <marco.oliva@upc.edu>
2021-12-15 17:01:33 +0100

    MAIN: Se comento lo referente al solver LAPACK, se agregaron las llamadas de las subrutinas incoporadas en library. Se quito nBVs y nBVscol y se pusieron como globales. Se definieron las variables NOFIX, IFPRE, PRESC, creadass por VinculBVs. Poner como globales y asignarlas aqui en MAIN. Se agregaron algunos print para verificar la comunicacion entre variables de subrutina y su declaracion global. Si mantienen el valor nband y nBVs.

--- main_CDR.f90
+++ main_CDR.f90
@@ -5,17 +5,21 @@ program main_CDR3d
   implicit none
   
   ! - - - - - - - - - - * * * Variables que se usan aqui en main * * * * * * * - - - - - - - - - -
-  double precision, allocatable, dimension(:,:) :: A_K, A_F, AK_LU, Solution, N, dN_dxi, dN_deta
-  double precision, dimension(3,nne)   :: Hesxieta
-  integer, allocatable, dimension(:,:) :: BVs
-  integer                              :: nBVs, nBVscol
-  real                                 :: start, finish
+  double precision, allocatable, dimension(:,:) :: A_K, A_F, presc
+  double precision, allocatable, dimension(:,:) :: AK_LU, Solution
+  double precision, allocatable, dimension(:,:) :: N, dN_dxi, dN_deta
+  double precision,            dimension(3,nne) :: Hesxieta
+  integer, allocatable, dimension(:,:)          :: BVs, ifpre
+  integer, allocatable, dimension(:)            :: nofix
+  real                                          :: start, finish
+
+  !integer                             :: nBVs, nBVscol      esta en el modulo como variable global
  
   !=============== S O L V E R ============================ 
-  external                             :: mkl_dgetrfnp, dgetrf, dgetrs
-  integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
-  integer, allocatable, dimension(:)   :: S_ipiv
-  character(len=1)                     :: S_trans
+  !external                             :: mkl_dgetrfnp, dgetrf, dgetrs
+  !integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
+  !integer, allocatable, dimension(:)   :: S_ipiv
+  !character(len=1)                     :: S_trans
   ! - - - - - - - - - - - - - - - * * * Fin * * * * * * * - - - - - - - - - - - - - - - - 
   call cpu_time(start)
 
@@ -25,18 +29,26 @@ program main_CDR3d
   call ReadTensors(30, File_tensors, difma, conma, reama, force)
 
   print*, '!=============== INFO DURING EXECUTION ===============!'
-  
   call GaussQuadrature(ngaus, weigp)
   call ShapeFunctions(ngaus, nne, N, dN_dxi, dN_deta, Hesxieta)  
-  
-  allocate(A_K(ntotv,ntotv), AK_LU(ntotv,ntotv) )
-  allocate(A_F(ntotv, 1), Solution(ntotv, 1))
-  call SetBounCond( nBVs, nBVscol) !Esta funcion crea el archivo bcsVP.dat
+ 
+  call SetBoundVal( nBVs, nBVscol) !Esta funcion crea el archivo BVs.dat
   allocate( BVs(nBVs, nBVscol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
-  call ReadIntegerFile(40,"BVs.dat", nBVs, nBVscol, BVs)!Llamo el archivo de valores en la frontera y lo guardo en BVs
-  
-  call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_F)
-  call ApplyBoundCond(nBVs, BVs, A_K, A_F )
+  print*, 'numero de BV from main', nBVs 
+  print*, 'numero de BV columns from main', nBVscol 
+  print*, ' '
+  call ReadIntegerFile(40,"BVs.dat", nBVs, nBVscol, BVs)!Lectura del archivo de valores en la frontera y lo guardo en BVs
+  allocate(A_K(nband+1,ntotv))
+  allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) ) 
+  allocate(AK_LU(nband+1,ntotv))
+  allocate(A_F(ntotv, 1), Solution(ntotv, 1))
+
+  call VinculBVs(  BVs, nofix, ifpre, presc )
+  call Prevop(A_K)
+
+  ! Despues de la llamada de prevop, se guarda el valor de nband que esta global en el modulo y se puede asignar dimension a A_K
+  !call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_F)
+  !call ApplyBVs(nofix,ifpre,presc,rigid,gload)
   
   print*, ' '
   print*, 'Shape of Global K: ',shape(A_K)
@@ -51,34 +63,34 @@ program main_CDR3d
   
   print*,' '
   print*,'!=============== SOLVER (LAPACK) ===============!'
-  S_m   = size(AK_LU,1)
-  S_n   = size(AK_LU,2)
-  S_lda = max(1,size(AK_LU,1)) ! lda ≥ max(1, n).
-  S_trans = 'N'
-  S_nrhs  = 1
-  S_ldb   = max(1,size(Solution,1))
-  allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
+  !S_m   = size(AK_LU,1)
+  !S_n   = size(AK_LU,2)
+  !S_lda = max(1,size(AK_LU,1)) ! lda ≥ max(1, n).
+  !S_trans = 'N'
+  !S_nrhs  = 1
+  !S_ldb   = max(1,size(Solution,1))
+  !allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
  
-  print*,'  •INITIALIZING LU FACTORIZATION A = P*L*U.....'
-  call dgetrf( S_m, S_n, AK_LU, S_lda, S_ipiv, S_infoLU )
-  call MKLfactoResult( S_infoLU )
+  !print*,'  •INITIALIZING LU FACTORIZATION A = P*L*U.....'
+  !call dgetrf( S_m, S_n, AK_LU, S_lda, S_ipiv, S_infoLU )
+  !call MKLfactoResult( S_infoLU )
 
-  print*,'  •SOLVING SYSTEM OF EQUATIONS..... '
-  call dgetrs( S_trans, S_n, S_nrhs, AK_LU, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
-  call MKLsolverResult( S_infoSOL )
-  
-  
-  print*, 'Writing postprocesses files.....'
-  call writeMatrix(A_K, 100, 'A_K.dat', A_F, 200, 'A_F.dat')
-  call writeMatrix(AK_LU, 300, 'AKLU.dat', Solution, 400, 'Sol.dat')
-  !call PosProcess(Solution, File_PostMsh, 'msh')
-  !call PosProcess(Solution, File_PostRes, 'res')
+  !print*,'  •SOLVING SYSTEM OF EQUATIONS..... '
+  !call dgetrs( S_trans, S_n, S_nrhs, AK_LU, S_lda, S_ipiv, Solution, S_ldb, S_infoSOL )
+  !call MKLsolverResult( S_infoSOL )
+  !
+  !
+  !print*, 'Writing postprocesses files.....'
+  !call writeMatrix(A_K, 100, 'A_K.dat', A_F, 200, 'A_F.dat')
+  !call writeMatrix(AK_LU, 300, 'AKLU.dat', Solution, 400, 'Sol.dat')
+  !!call PosProcess(Solution, File_PostMsh, 'msh')
+  !!call PosProcess(Solution, File_PostRes, 'res')
 
-  DEALLOCATE( S_ipiv)
+  !DEALLOCATE( S_ipiv)
   DEALLOCATE( A_F)
   DEALLOCATE( A_K)        
-  DEALLOCATE( AK_LU)
-  DEALLOCATE( Solution)
+  !DEALLOCATE( AK_LU)
+  !DEALLOCATE( Solution)
   call cpu_time(finish)
   print '(A11,f9.2,A8)',' CPU-Time =', finish-start, ' Seconds'
   print*, ' ' 
