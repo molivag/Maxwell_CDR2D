5312e83a6798b6a38eb74b479f1dec90a1bdcf30
M_OlivaG <marco.oliva@upc.edu>
2022-01-05 17:27:53 +0100

    MAIN: Se llama a inputData se imprimen los tensores dependiendo del dof. Se imprime en archivo la matriz global antes y despues de aplicar las condiciones de frontera. La matriz sale singular despues del LU.

--- main_CDR.f90
+++ main_CDR.f90
@@ -8,47 +8,102 @@ program main_CDR3d
   double precision, allocatable, dimension(:,:) :: A_K, A_F, presc
   double precision, allocatable, dimension(:,:) :: AK_LU, Solution
   double precision, allocatable, dimension(:,:) :: N, dN_dxi, dN_deta
-  double precision,            dimension(3,nne) :: Hesxieta
+  double precision,            dimension(3,4) :: Hesxieta
   integer, allocatable, dimension(:,:)          :: BVs, ifpre
   integer, allocatable, dimension(:)            :: nofix
   real                                          :: start, finish
 
   !=============== S O L V E R ============================ 
   external                             :: mkl_dgetrfnp, dgetrf, dgetrs
-  integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL
+  integer                              :: S_m, S_n, S_lda, S_ldb, S_infoLU, S_nrhs , S_infoSOL, i,j,k,l
   integer, allocatable, dimension(:)   :: S_ipiv
   character(len=1)                     :: S_trans
   ! - - - - - - - - - - - - - - - * * * Fin * * * * * * * - - - - - - - - - - - - - - - - 
   call cpu_time(start)
-
+  
+  call inputData ( )
   call GeneralInfo( )
-  call ReadIntegerFile(10,File_element, nelem, nne + 1, lnods)  
-  call ReadRealFile(20,File_coord, nnodes,3, coord) 
-  call ReadTensors(30, File_tensors, difma, conma, reama, force)
+  call ReadTensors(30, difma, conma, reama, force)
 
+  print*,'size of difma', size(difma)
+  print*,'size of conma', size(conma)
+  print*,'size of reama', size(reama)
+
+
+  !print*,'nelem ', nelem
+  !print*,'nnodes', nnodes
+  !print*,'nne   ', nne
+  !print*,'ndofn ', ndofn
+  !print*,'totGp ', totGp
+  !print*,'kstab ', kstab
+  !print*,'ktaum ', ktaum
+  !print*,'ntotv ', ntotv
+  !print*,'nevab ', nevab
+  !print*,'maxban', maxband
+  !! print*,'#........'
+  !print*,'hnatu', hnatu
+  !print*,'patau', patau
+  !print*,'ElemType ',ElemType
+  !print*,' '
+  !
+  !do i =1, nelem
+  !  print*, (lnods(i,j), j=1,nne+1)
+  !enddo
+
+  !print*, ' '
+
+  !do i =1, nnodes
+  !  print*, (coord(i,j), j=1,DimPr+1)
+  !enddo
   print*, '!=============== INFO DURING EXECUTION ===============!'
   call GaussQuadrature(ngaus, weigp)
   call ShapeFunctions(ngaus, nne, N, dN_dxi, dN_deta, Hesxieta)  
- 
+
+  print*, ' '
+  print*, 'Diffusion matrix'
+  do i = 1,dimPr
+    do j = 1,DimPr
+      print"(A,2I1)", 'k_',i,j
+      do k = 1,ndofn
+        print*,( difma(k,l,i,j), l=1,ndofn)
+      end do
+      print*,' '
+    end do
+  end do
+ print*, 'Convection matrix'
+  do k = 1, DimPr
+    print"(A,2I1)",'A_',k
+    do i = 1, ndofn
+      write(*, *)( conma(i,j,k) ,j=1, ndofn)
+    end do
+    print*,''
+  end do
+  print*,'Reaction'
+  do i=1,ndofn
+    write(*, *)( reama(i,j) ,j=1,ndofn)
+  end do
+  print*,'force'
+  do i =1, ndofn
+    print*, force(i)
+  end do
+
   call SetBoundVal( nBVs, nBVscol) !Esta funcion crea el archivo BVs.dat
   allocate( BVs(nBVs, nBVscol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
-  print*, 'numero de BV from main', nBVs 
-  print*, 'numero de BV columns from main', nBVscol 
-  print*, ' '
   call ReadIntegerFile(40,"BVs.dat", nBVs, nBVscol, BVs)!Lectura del archivo de valores en la frontera y lo guardo en BVs
   allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) ) 
   allocate(A_F(ntotv, 1), Solution(ntotv, 1))
 
   call VinculBVs(  BVs, nofix, ifpre, presc )
 
-  !Despues de la llamada de prevop, se guarda el valor de nband que esta global en el modulo y se puede asignar dimension a A_K
+  !el allocate de A_K esta dentro de GlobalSystem, primero calcula el semiancho de banda, luego aloja A_K y luego calcula Ke y rhslo
   call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_F)
+  print*, 'Shape of Global K: ',shape(A_K)
+  call writeMatrix(A_K, 100, 'A_K.dat', A_F, 200, 'A_F.dat')
   call ApplyBVs(nofix,ifpre,presc,A_K,A_F)
+  call writeMatrix(A_K, 111, 'A_Kbv.dat', A_F, 222, 'A_Fbv.dat')
   
   print*, ' '
-  print*, 'Shape of Global K: ',shape(A_K)
   print*, 'Shape of Global F: ',shape(A_F)
-  print*, 'nband desde main:  ',nband
   allocate(AK_LU(nband+1,ntotv))
   Solution = A_F !Solucion sera reescrito por la solucion de lapack asi no reescribo el vector global.
   AK_LU    = A_K
@@ -57,7 +112,7 @@ program main_CDR3d
   DEALLOCATE(dN_deta)
   DEALLOCATE(BVs)
   deallocate(nofix, ifpre, presc)
-
+  
   print*,' '
   print*,'!=============== SOLVER (LAPACK) ===============!'
   S_m   = size(AK_LU,1)
@@ -67,7 +122,6 @@ program main_CDR3d
   S_nrhs  = 1
   S_ldb   = max(1,size(Solution,1))
   allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
- 
   print*,'  •INITIALIZING LU FACTORIZATION A = P*L*U.....'
   call dgetrf( S_m, S_n, AK_LU, S_lda, S_ipiv, S_infoLU )
   call MKLfactoResult( S_infoLU )
@@ -78,7 +132,6 @@ program main_CDR3d
   
   
   print*, 'Writing postprocesses files.....'
-  call writeMatrix(A_K, 100, 'A_K.dat', A_F, 200, 'A_F.dat')
   call writeMatrix(AK_LU, 300, 'AKLU.dat', Solution, 400, 'Sol.dat')
   !call PosProcess(Solution, File_PostMsh, 'msh')
   !call PosProcess(Solution, File_PostRes, 'res')
