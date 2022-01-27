38e1397895648e0efffb2e59fb38a018e4195779
M_OlivaG <marco.oliva@upc.edu>
2022-01-27 15:58:26 +0100

    MAIN: Se renombraron algunas variables en la delcaracion de variables, se dejo una sola matriz global A_K quitando A_Kbv. Se reestructuro toda la parte del solver dejando solo las rutinas de lapack, el solver de retpla se dejo comentado y en general se ordeno la rutina. Todo funciona al 100

--- main_CDR.f90
+++ main_CDR.f90
@@ -2,31 +2,24 @@ program main_CDR3d
   use library
   use param
   use biunit 
-  use solver
+  !use solver
   !use boundVal
 
   implicit none
   
   ! - - - - - - - - - - * * * Variable declaration * * * * * * * - - - - - - - - - -!
-  double precision, allocatable, dimension(:,:) :: A_K, A_Kbv, A_F, Solution, presc
+  double precision, allocatable, dimension(:,:) :: A_K, A_F,  presc
   double precision, allocatable, dimension(:,:) :: N, dN_dxi, dN_deta
   double precision,              dimension(3,4) :: Hesxieta
   integer, allocatable, dimension(:,:)          :: BVs, ifpre
   integer, allocatable, dimension(:)            :: nofix
   real                                          :: start, finish
   ! - - - - - - - - * * * Variable declaration (SOLVER) * * * * * * * - - - - - - - !
- ! external                             :: 
- ! integer                              ::
- ! integer, allocatable, dimension(:)   :: 
- ! character(len=1)                     :: 
-  
-    external :: dgbtrf, dgbtrs, dpbtrs, dpbtrf
-    integer, allocatable :: ipiv(:)
-    integer :: i, j, k, kl, ku, ldab, S_m, S_n, nrhs, info, ldb
-    double precision, allocatable :: ab(:, :), ablap(:, :), A_Ktot(:,:), AKb_LU(:,:), b(:,:)
-    character(len=1) :: trans, uplo
-  != = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-
+  external :: dgbtrf, dgbtrs, dpbtrs, dpbtrf
+  integer, allocatable, dimension(:) :: S_ipiv
+  integer :: S_m, S_n, S_nrhs, info, S_ldSol
+  double precision, allocatable, dimension(:,:) :: AKbLU, Sols
+  character(len=1) :: S_trans, S_uplo
 
   !---------- Input Data -----------!
   call cpu_time(start)
@@ -41,151 +34,78 @@ program main_CDR3d
   !---------- Global Matrix and Vector -----------!
   !the allocate of A_K is inside of GlobalSystem, first compute the semi bandwidth, then allocate A_K
   call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_F)
-  call writeMatrix(A_K, 10, 'A_K.dat', A_F, 20, 'A_F.dat')
 
   !---------- Boundary Conditions -----------!
   call SetBoundVal( nBVs, nBVscol) !Esta funcion crea el archivo BVs.dat
-  print*, 'nbvs', nbvs
-  print*, 'nbvscol', nbvscol
   allocate( BVs(nBVs, nBVscol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
-  call ReadIntegerFile(30,"BVs.dat", nBVs, nBVscol, BVs)!Lectura del archivo de boundary values will stored at BVs
+  call ReadIntegerFile(30,"BVs.dat", nBVs, nBVscol, BVs)!Reading of boundary values file will store at BVs
   allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) ) 
-  allocate(A_Kbv(nband+1,ntotv))
-  A_Kbv = A_K
   call VinculBVs(  BVs, nofix, ifpre, presc )
-  call ApplyBVs(nofix,ifpre,presc,A_Kbv,A_F )
-  call writeMatrix(A_Kbv, 40, 'A_Kbv.dat', A_F, 50, 'A_Fbv.dat')
-  !write(*,"(4F10.5)") ((A_K(i,j), j=1,S_n), i=1,nband+1)  
- print*, 'nabnd',nband 
+  call ApplyBVs(nofix,ifpre,presc,A_K,A_F )
+
   !---------- Memory Relase -----------!
   DEALLOCATE(N)
   DEALLOCATE(dN_dxi)
   DEALLOCATE(dN_deta)
   DEALLOCATE(BVs)
   DEALLOCATE(nofix, ifpre, presc)
+
+
+  !---------- Solving System of Equations by MKL library -----------!
+  write(*,*) ''
+  print*,'!================= MKL <S>OLVER ===============!'
+  S_m     = ntotv       !size(AK,1)
+  S_n     = ntotv       !size(AK,2)
+  S_ldSol = max(1,S_n)
+  S_trans = 'N'
+  S_uplo  = 'U'
+  S_nrhs  = 1
+
+
+  allocate( AKbLU(ldAKban,ntotv))
+  allocate( Sols(S_ldSol, 1))
+  allocate( S_ipiv(max(1,min(S_m, S_n)) ) ) !size (min(m,n))
   
+  AKbLU = A_K         !AK_band(ldab,*) The array AK_band contains the matrix A_K in band storage
+  Sols  = A_F         !Sol_vec will be rewrited by LAPACK solution avoiding lose A_F
+
+  print*,'  •INITIALIZING BAND LU DECOMPOSITION.....'
+  !print*, 'Cholesky-decomposition of band-storaged Matrix'
+  !call dpbtrf( uplo, S_n, nband, AKb_LU, ldAKban, info )
+  call dgbtrf(S_m, S_n, lowban, upban, AKbLU, ldAKban, S_ipiv, info)
+  call MKLfactoResult('dgbtrf',info)
+  
+  print*,'  •SOLVING SYSTEM OF EQUATIONS..... '
+  !call dpbtrs( uplo, S_n, nband, 1, AKb_LU, ldAKban, b, ldb, info )
+  call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AKbLU, ldAKban, S_ipiv, Sols, S_ldSol, info )
+  call MKLsolverResult('dgbtrs',info)
+  
+
   !---------- Solving System of Equations -----------!
 
-  allocate(Solution(ntotv, 1))
-  Solution = 0.0
-  call solsistem(A_Kbv,A_Kbv,1,A_F,Solution)
+  !Sols = 0.0
+  !call solsistem(A_K,A_K,S_nrhs,A_F,Sols)
   
-  != = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
-  write(*,*)
-  write(*,*)
-  S_m=ntotv
-  S_n=ntotv
-  kl =0
-  ku = nband
-  ldab = 2*kl + ku + 1
-  trans= 'N'
-  uplo = 'U'
-  nrhs = 1
-  ldb  = max(1, S_n)
-  
-  allocate(A_Ktot(S_m,S_n), ab(ldab,S_n), ablap(ldab,S_n), ipiv(S_n), AKb_LU(ldab,S_n), b(ldb,1))
-  !    Read A from data file
-  ab     = 0.0
-  ablap  = 0.0
-  AKb_LU = 0.0
-  ipiv   = 0 
-  
-  open(unit=1010, file= 'globalK.dsc', STATUS="old", ACTION="read") 
-  read(1010,*) ((A_Ktot(i,j), j=1,S_n), i=1,S_m) 
-  !
-  close(1010)
-  !print*, ''
-  !print("(A,2(I2,2x))"), 'The general banded square matrix A_Ktot of shape:', shape(A_Ktot)
-  !write(*,"(9F10.5)") ((A_Ktot(i,j), j=1,S_n), i=1,S_m)  
-
-  !print*, ' '
-print("(A,2(I2,2x))"),'Transforming the general banded matrix to general-band storage mode of shape: ', shape(ab)
-  k   = kl + ku + 1
-  do j=1,S_n
-     do  i=max(1, j-ku),min(S_n,j+kl)!i=max(j-ku,1),min(j+kl,S_n)
-       ablap(i-j+k,j)=a_ktot(i,j)
-     end do
-   end do
-  write(*,"(9F10.5)") ((ablap(i,j), j=1,S_n), i=1,ldab)  
-  print*, ' '
-  print*, 'Asi como arriba debe salir la siguiente matriz: '
-  print("(A,2(I3,2x))"), 'The banded matrix from retpla.f whit shape:', shape(A_K)
-  write(*,"(9F10.5)") ((A_K(i,j), j=1,ntotv), i=1,nband+1)  
-
-  !print*, ' '
-  !print*, 'Reordering the band matrix from example file to LAPACK format'
-  !i=0
-  !j=0
-  !do k = nband+1, 1,-1
-  !  i = i+1
-  !  j = 1
-  !  do l = ntotv,1,-1
-  !    ab(i,j) = A_Ktot(k,l)
-  !    j=j+1
-  !  end do
-  !end do
-  !write(*,"(9F10.5)") ((ab(i,j), j=1,S_n), i=1,ldab)  
-
-  write(*,*)
-  write(*,*)
-  write(*,*)
-  stop
-
-  AKb_LU = ab
-  
-  write(*,*)
-  !call dpbtrf( uplo, S_n, nband, AKb_LU, ldab, info )
-  call dgbtrf(S_m, S_n, kl, ku, AKb_LU, ldab, ipiv, info)
-
-  !print*, 'info Cholesky decomposition' , info
-  !print*, 'Cholesky-decomposition of band-storaged Matrix'
-  print*, 'info LU decomposition' , info
-  !print*, 'LU-decomposition of band-storaged Matrix'
-
-  !write(*,"(9F10.5)") ((AKb_LU(i,j), j=1,S_n), i=1,ldab)  
-  !Write (*, *)
-  !Write (*, *) 'IPIV: Permutation'
-  !Write (*, 100) ipiv(1:min(S_m,S_n))
-  If (info/=0) Then
-    Write (*, *) 'The factor U is singular'
-  End If
-  
-  print*, 'Iniciando solver' 
-  b = A_F
-  !call dpbtrs( uplo, S_n, nband, 1, AKb_LU, ldab, b, ldb, info )
-  call dgbtrs( trans, S_n, kl, ku, nrhs, AKb_LU, ldab, ipiv, b, ldb, info )
-  print*, 'info solver' , info
-  !print*, 'Solution'
-  !write(*,"(1F15.5)") (b(i,1), i=1,S_n)  
-
-  100 Format(I11)
 
 
-
-
-
-
-
-
-
-
-  != = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
   !---------- Print and write results -----------!
-  !print*, 'Writing postprocesses files.....'
-  !call writeMatrix(A_K, 60, '-.dat', Solution, 70, 'SolCholesky.dat')
-  !print*, ' ' 
+  write(*,*)
+  print*,'!=============== Output Files ================!'
+  call writeMatrix(A_K, 10, 'A_K.dat', A_F, 20, 'A_F.dat')
+  call writeMatrix(AKbLU,60,'AKbLU.dat', Sols, 70, 'SolMKL_LU.dat')
+  print*, ' ' 
   print*, 'Shape of Global K: ',shape(A_K)
   print*, 'Shape of Global F: ',shape(A_F)
-  print*,' Shape of Solution: ',shape(Solution)
+  print*, 'Shape of Solution: ',shape(Sols)
   !call PosProcess(Solution, File_PostMsh, 'msh')
   !call PosProcess(Solution, File_PostRes, 'res')
 
   !---------- Memory Relase -----------!
   DEALLOCATE( A_F)
-  DEALLOCATE( A_K)        
-  DEALLOCATE( A_Kbv)        
-  DEALLOCATE( Solution)
+  DEALLOCATE( A_K)
+  DEALLOCATE( AKbLU)
+  DEALLOCATE( Sols)
   call cpu_time(finish)
-  write(*,'(A11,f9.2,A8)'),' CPU-Time =', finish-start, ' Seconds', ' '
+  write(*,'(A11,f9.2,A8)')' CPU-Time =', finish-start, ' Seconds', ' '
 
 end program main_CDR3d
