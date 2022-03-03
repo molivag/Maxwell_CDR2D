da6ccc8f768085bef08d48505a4734a27f40d2ab
Marco O.G <marco.oliva@upc.edu>
2022-03-03 18:23:16 +0100

    MAIN, se reestructuro el orden del programa para incorporar el caso estatico y transitorio mediante la variable problem_type, se definen un if para que dependiendo del valor de problem_type se resuelva uno u otro propblema en el caso de trans se agrego en el if las variables time para los pasos de tiempomanda y se llama la rutina BackwardEuler

--- main_CDR.f90
+++ main_CDR.f90
@@ -1,19 +1,20 @@
 program main_CDR3d
-  use library; use param ; use biunit ; use boundVal
+  use library; use param ; use biunit ; use boundVal ; use timeInt
   !use solver
 
 implicit none
 
   ! - - - - - - - - - - * * * Variable declaration * * * * * * * - - - - - - - - - -!
-  double precision, allocatable, dimension(:,:) :: A_K, A_F,  presc
+  double precision, allocatable, dimension(:,:) :: A_K, A_F, A_C, presc
   double precision, allocatable, dimension(:,:) :: N, dN_dxi, dN_deta
   double precision,              dimension(3,4) :: Hesxieta
   integer, allocatable, dimension(:,:)          :: BVs, ifpre
   integer, allocatable, dimension(:)            :: nofix
-  real                                          :: start, finish
+  real                                          :: start, finish, time_ini, time_fin, max_time, u0_cond
+  character(len=5)                              :: problem_type
   ! - - - - - - - - * * * Variable declaration (SOLVER) * * * * * * * - - - - - - - !
   external :: dgbtrf, dgbtrs, dpbtrs, dpbtrf
-  double precision, allocatable, dimension(:,:) :: AKbLU, Sols
+  double precision, allocatable, dimension(:,:) :: AK_LU, u_sol
   integer, allocatable, dimension(:) :: S_ipiv
   integer :: S_m, S_n, S_nrhs, info, S_ldSol
   character(len=1) :: S_trans, S_uplo
@@ -22,75 +23,92 @@ implicit none
   call cpu_time(start)
   call inputData( )
   call GeneralInfo( )
-
-  !---------- Shape Functions -----------!
+  problem_type = 'tran'
+  !              'stat'              
+  !---------- Shape Functions ---------------!
   !print*, '!=============== INFO DURING EXECUTION ===============!'
   call GaussQuadrature(ngaus, weigp)
   call ShapeFunctions(ngaus, nne, N, dN_dxi, dN_deta, Hesxieta)
 
-  !---------- Global Matrix and Vector -----------!
+  !---------- Global Matrix and Vector ------!
   !the allocate of A_K is inside of GlobalSystem, first compute the semi bandwidth, then allocate A_K
-  call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_F)
+  call GlobalSystem(N, dN_dxi, dN_deta, Hesxieta, A_K, A_C, A_F)
   !print*, 'Antes de BV'
   !write(*,"(I2,1x, 16F13.9)") (i, (A_K(i,j), j=1,ntotv), i=1,ldAKban)
+  !print*,'!=============== Output Files ================!'
+  !call writeMatrix(A_K, 10, 'A_K.dat', A_F, 20, 'A_F.dat')
+  !call writeMatrixAKbLU,60,'-', Sols, 70, 'SolMKL_LU.dat')
+  
+  !---------- Memory Relase -----------!
+  DEALLOCATE( N, dN_dxi, dN_deta)
 
-  !---------- Boundary Conditions -----------!
+  !------- Setting Boundary Conditions ------!
   call SetBoundVal( nBVs, nBVscol) !Esta funcion crea el archivo BVs.dat
   allocate( BVs(nBVs, nBVscol) ) !Designo la memoria para la matriz de nodos con valor en la frontera
   call ReadIntegerFile(30,"BVs.dat", nBVs, nBVscol, BVs)!Reading of boundary values file will store at BVs
-  allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) )
+  allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) ) !memoria para el numero de nodo, si esta preescrito y su valor
   call VinculBVs(  BVs, nofix, ifpre, presc )
-  call ApplyBVs(nofix,ifpre,presc,A_K,A_F )
 
-  !print*, 'Despues de BV'
-  !write(*,"(I2,1x, 16F13.9)") (i, (A_K(i,j), j=1,ntotv), i=1,ldAKban)
-
-  !---------- Memory Relase -----------!
-  DEALLOCATE(N, dN_dxi, dN_deta, BVs, nofix, ifpre, presc)
-
+  !----- Setting MKL-Solver Parammeters -----!
   write(*,*) ''
   print*,'!================= MKL <S>OLVER ===============!'
   S_m     = size(A_K,2)  !antes ntotv
-  S_n     = size(A_K,2)   !antes ntotv
+  S_n     = size(A_K,2)  !antes ntotv
   S_ldSol = max(1,S_n)
   S_trans = 'N'
   S_uplo  = 'U'
   S_nrhs  = 1
-  allocate( AKbLU(ldAKban,ntotv))
-  allocate( Sols(S_ldSol, 1))
-  allocate( S_ipiv(max(1,min(S_m, S_n)) ) ) !size (min(m,n))
-  AKbLU = A_K                               !AK_band(ldab,*) The array AK_band contains the matrix A_K in band storage
-  Sols  = A_F                               !Sol_vec will be rewrited by LAPACK solution avoiding lose A_F
 
-  !---------- Solving System of Equations by retpla solver -----------!
-  !  print*, 'Cholesky-decomposition of band-storaged Matrix'
-  !  call solsistem(A_K,A_K,1,A_F,Sols)
-  print*,'  •INITIALIZING BAND LU DECOMPOSITION.....'
-  !print*, 'Cholesky-decomposition of band-storaged Matrix'
-  !call dpbtrf( uplo, S_n, nband, AKb_LU, ldAKban, info )
-  call dgbtrf(S_m, S_n, lowban, upban, AKbLU, ldAKban, S_ipiv, info)
-  call MKLfactoResult('dgbtrf',info)
+  !-------- Problem Type Definition --------!
+  if(problem_type .eq. 'tran')then
+    
+    time_ini = 0.0   !Estos valores
+    time_fin = 0.5       !Deben ser leidos en
+    max_time = 10.0           !el archivo de entrada
+    u0_cond  = 0.0  
+    call BackwardEuler(time_ini, time_fin, max_time, u0_cond,&
+    &                  nofix, ifpre, presc, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol , A_K, A_C, A_F)
+    DEALLOCATE( BVs,  nofix, ifpre, presc)
+  else
+    call ApplyBVs(nofix,ifpre,presc,A_K, A_F)
+    !print*, 'Despues de BV'
+    !write(*,"(I2,1x, 16F13.9)") (i, (A_K(i,j), j=1,ntotv), i=1,ldAKban)
+    !---------- Memory Relase -----------!
+    DEALLOCATE(BVs, nofix, ifpre, presc)
+    
+    allocate( AK_LU(ldAKban,ntotv))
+    allocate( u_sol(S_ldSol,1))
+    allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
+    
+    AK_LU = A_K                 !AK_band(ldab,*) The array AK_band contains the matrix A_K in band storage
+    u_sol = A_F                 !Sol_vec will be rewrited by LAPACK solution avoiding lose A_F
+    !---------- Solving System of Equations by retpla solver -----------!
+    !  print*, 'Cholesky-decomposition of band-storaged Matrix'          
+    !  call solsistem(A_K,A_K,1,A_F,Sols)                                                             
+    print*,'  •INITIALIZING BAND LU DECOMPOSITION.....'                                                    
+    !print*, 'Cholesky-decomposition of band-storaged Matrix'     
+    !call dpbtrf( uplo, S_n, nband, AKb_LU, ldAKban, info )
+    call dgbtrf(S_m, S_n, lowban, upban, AK_LU, ldAKban, S_ipiv, info)  
+    call MKLfactoResult('dgbtrf',info)  !Aqui agregar el paso del tiempo para en cada tiempo indicar el info de ejecucion
+    print*,'  •SOLVING SYSTEM OF EQUATIONS..... '
+    !call dpbtrs( uplo, S_n, nband, 1, AKb_LU, ldAKbn, b, ldb, info )                                     
+    call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AK_LU, ldAKban, S_ipiv, u_sol, S_ldSol, info )
+    call MKLsolverResult('dgbtrs',info) !Aqui agregar el paso del tiempo para en cada tiempo indicar el info de ejecucion
+    !---------- Print and write results -----------!
+    call PosProcess(u_sol, File_PostMsh, 'msh') !se debe agregar el nt como dummyvariable
+    call PosProcess(u_sol, File_PostRes, 'res')
+    
+    print*, ' '
+    print*, 'Shape of Global K: ',shape(A_K)
+    print*, 'Shape of Global F: ',shape(A_F)
+    print*, 'Shape of Solution: ',shape(u_sol)
+    write(*,*)
+    DEALLOCATE( A_F, A_K, AK_LU, u_sol)
+    
+  endif
 
-  print*,'  •SOLVING SYSTEM OF EQUATIONS..... '
-  !call dpbtrs( uplo, S_n, nband, 1, AKb_LU, ldAKban, b, ldb, info )
-  call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AKbLU, ldAKban, S_ipiv, Sols, S_ldSol, info )
-  call MKLsolverResult('dgbtrs',info)
-
-  !---------- Print and write results -----------!
-  print*,'!=============== Output Files ================!'
-  !call writeMatrix(A_K, 10, 'A_K.dat', A_F, 20, 'A_F.dat')
-  !call writeMatrix(AKbLU,60,'-', Sols, 70, 'SolMKL_LU.dat')
-  print*, ' '
-  print*, 'Shape of Global K: ',shape(A_K)
-  print*, 'Shape of Global F: ',shape(A_F)
-  print*, 'Shape of Solution: ',shape(Sols)
-  write(*,*)
-  call PosProcess(Sols, File_PostMsh, 'msh')
-  call PosProcess(Sols, File_PostRes, 'res')
-  write(*,*)
 
   !---------- Memory Relase -----------!
-  DEALLOCATE( A_F, A_K, AKbLU, Sols)
   call cpu_time(finish)
   write(*,'(A11,f9.2,A8)')' CPU-Time =', finish-start, ' Seconds', ' '
 
