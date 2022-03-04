bc1c9dcc4e3500fb6613447de5ec00aff7d4ccbf
Marco O.G <marco.oliva@upc.edu>
2022-03-04 20:48:54 +0100

    En BackWardEuler se incluyo la rutina GlobalSystem_time que ensambla A_F usando la operacion gather para la contribucion de la matriz de capacidad y u en el tiempo presente, al resolverse el sistema global en cada paso de tiempo se incluyeron las variables necesarias para hacerlo. Hay un erro entre library y aqui: ver bitacora

--- mod_timeInt.f90
+++ mod_timeInt.f90
@@ -1,6 +1,6 @@
 module timeInt
   use param
-  use library, only: ApplyBVs, PosProcess, MKLsolverResult, MKLfactoResult
+  use library, only: ApplyBVs, GlobalSystem_Time, PosProcess, MKLsolverResult, MKLfactoResult
 
   contains
 
@@ -34,22 +34,23 @@ module timeInt
     end subroutine initialCondition
     
     
-    subroutine BackwardEuler(time_ini, time_fin, max_time, u0_cond, nofix, ifpre, presc,&
-      &                      S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol , A_K, A_C, A_F)
+    subroutine BackwardEuler(N, dN_dxi, dN_deta, Hesxieta, time_ini, time_fin, max_time, u0_cond,&
+      &                      nofix, ifpre, presc, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol )
       
       implicit none
       
       external :: dgbtrf, dgbtrs
       
-      double precision, dimension(ldAKban,ntotv), intent(in) :: A_K, A_C
+      double precision, dimension(nne,TotGp), intent(in)     :: N, dN_dxi, dN_deta
+      double precision, dimension(3,nne), intent(in)         :: Hesxieta
       double precision, dimension(ndofn,nBVs), intent(in)    :: presc
-      double precision, dimension(ntotv,1), intent(in)       :: A_F
       integer, dimension(ndofn,nBVs), intent(in)             :: ifpre
       integer, dimension(nBVs), intent(in)                   :: nofix
       character(len=1), intent(in)                           :: S_trans
       integer,intent (in)                                    :: S_m, S_n, S_nrhs, S_ldSol
       real, intent(in)                                       :: time_ini, time_fin, max_time, u0_cond
       ! - - Local Variables - -!
+      double precision, allocatable, dimension(:,:) :: A_K, A_C, A_F
       double precision, allocatable, dimension(:,:) :: AK_time, u_pre, u_fut, rhs_time, u_init
       integer, allocatable, dimension(:)            :: S_ipiv
       double precision :: delta_t
@@ -57,27 +58,16 @@ module timeInt
       real             :: nt 
       
       
-      ! do time=I(3)+k:k:I(4)
-      !   t=time;     
-      !   fnew=(eval(f ))
-      !   fnew=h*fnew
-      !   fnew=[bc(1); fnew; bc(2)]
-      !   b=theta*fnew+(1-theta)*fold+B*uold
-      !   y=L\b
-      !   u=U\y
-      !   uold=u
-      ! end
-      
-      
       allocate( AK_time(ldAKban,ntotv))
-      allocate( rhs_time(ntotv,1), u_init(ntotv,1), u_pre(S_ldSol, 1), u_fut(S_ldSol, 1))
+      allocate( rhs_time(ntotv,1), u_init(ntotv,1) )
+      allocate(u_pre(S_ldSol, 1))
+      allocate( u_fut(S_ldSol, 1))
       allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
       
       ! call initial_condition ( node_num, node_xy, u0_cond, u_init ) !could be 0
       delta_t  = ( time_fin - time_ini ) / max_time     !Step size
       u_init = 0.0                                      !Para prueba lo dejo sin subroutina
       u_pre  = u_init                                   !u in present time 
-      u_fut  = 0.0                                      !here mkl will rewrite u_fut by the solution vector
       time   = 0                                        !initializing the time
       
       write ( *, '(a)' ) ' '
@@ -85,19 +75,25 @@ module timeInt
       write ( *, '(a,g14.6)' ) '  Final time =   ', time_fin
       write ( *, '(a,g14.6)' ) '  Step size =    ', delta_t
       write ( *, '(a,g14.6)' ) '  Number of steps = ', max_time
+      write ( *, '(a)' ) ' '
       
       print'(A6,F10.3,A)','time: ',time_ini,'is equal to the initial condiction'
+      write ( *, '(a)' ) ' '
       print*, 'Starting time-stepping. . . . .'
+      write ( *, '(a)' ) ' '
       do nt = time_ini+delta_t,time_fin,delta_t
         
         time = time + 1
+        
+        call GlobalSystem_Time(N, dN_dxi, dN_deta, Hesxieta, S_ldsol, delta_t, u_pre, A_K, A_C, A_F)
         print'(A11,I4,A2,F10.3,A2,F10.3,A)','time step: ',time,'= ',nt,'of ',time_fin,'seg'
         
         !-------- Implicit Scheme --------!
         AK_time  = 1/delta_t*A_C + A_K
-        rhs_time = A_F +  (u_pre * 1/delta_t)
+        rhs_time = A_F 
         ! rhs_time = A_F +  (A_C * u_pre * 1/elta_t)
         call ApplyBVs(nofix,ifpre,presc,AK_time,rhs_time)
+        u_fut  = A_F                                      !here mkl will rewrite u_fut by the solution vector
         
         !------- Factorizing Matrix -------!
         call dgbtrf(S_m, S_n, lowban, upban, AK_time, ldAKban, S_ipiv, info)
