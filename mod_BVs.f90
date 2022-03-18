43df47893138d5200716ac51802181a8a9319849
Marco O.G <marco.oliva@upc.edu>
2022-03-18 14:33:19 +0100

    BVs: Se modifico las condiciones a 10 izq 2 derecha para prueba de calidad

--- mod_BVs.f90
+++ mod_BVs.f90
@@ -121,28 +121,28 @@ module BoundVal
           x=coord(i,2)
           y=coord(i,3)
           if(y.eq.ymax) then 
-            if(x.eq.xmax)then            
-              write(100,70) i, 1, real(0)       !right top corner
-            elseif(x.eq.xmin)then               
-              write(100,70) i, 1, real(1)       !left top corner 
+            if(x.eq.xmax)then           
+              write(100,70) i, 1, real(2.200)   !right top corner
+            elseif(x.eq.xmin)then
+              write(100,70) i, 1, real(10.50)   !left top corner 
             else                                                     
               write(100,70) i, 0, real(0)       !top edge                      
             end if
             a = a+1
           else if (y.eq.ymin)then
             if(x.eq.xmin)then
-              write(100,70) i, 1, real(1)       !left bottom corner
+              write(100,70) i, 1, real(10)      !left bottom corner
             elseif(x.eq.xmax)then                                   
-              write(100,70) i, 1, real(0)       !right bottom corner
+              write(100,70) i, 1, real(2)       !right bottom corner
             else
               write(100,70) i, 0, real(0)       !botomm edge                     
             end if
             b = b+1
           else if(x.eq.xmax)then                                     
-              write(100,70) i, 1, real(0)       !right edge
+              write(100,70) i, 1, real(2)       !right edge
             d = d+1
           else if (x.eq.xmin)then                                    
-              write(100,70) i, 1, real(1)       !left edge
+              write(100,70) i, 1, real(10)       !left edge
             c = c+1
            
           end if
