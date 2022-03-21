50a48bc4df39e9ba3189aca05b7c1f88bccecdc2
Marco O.G <marco.oliva@upc.edu>
2022-03-21 14:57:09 +0100

    TIME: Se cambio de real a int max_time y se arreglo el loop de tiempo de nt a time quitando el warning de hacer loops reales; se aghrego la rutina dgbrfs para el refinado de la solucion pero se dejo comentado, se agregaron las variables correspondientes; se inicializaron algunas variables; el do del time-stepping se cambio de nt(real) a time(integer) evitando el warning que salia; se quitaron espacios en la impresion de pantalla y se paso hasta el final del modulo las lineas del refinado. No funciona bien.

--- mod_timeInt.f90
+++ mod_timeInt.f90
@@ -35,11 +35,11 @@ module timeInt
     
     
     subroutine BackwardEuler(N, dN_dxi, dN_deta, Hesxieta, time_ini, time_fin, max_time, u0_cond,&
-      &                      nofix, ifpre, presc, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol )
+      &                      nofix, ifpre, presc, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol, workdim)
       
       implicit none
       
-      external :: dgbtrf, dgbtrs
+      external :: dgbtrf, dgbtrs, dgbrfs
       
       double precision, dimension(nne,TotGp), intent(in)     :: N, dN_dxi, dN_deta
       double precision, dimension(3,nne), intent(in)         :: Hesxieta
@@ -47,81 +47,103 @@ module timeInt
       integer, dimension(ndofn,nBVs), intent(in)             :: ifpre
       integer, dimension(nBVs), intent(in)                   :: nofix
       character(len=1), intent(in)                           :: S_trans
-      integer,intent (in)                                    :: S_m, S_n, S_nrhs, S_ldSol
-      real, intent(in)                                       :: time_ini, time_fin, max_time, u0_cond
+      integer,intent (in)                                    :: S_m, S_n, S_nrhs, S_ldSol, max_time
+      real, intent(in)                                       :: time_ini, time_fin, u0_cond
       ! - - Local Variables - -!
       double precision, allocatable, dimension(:,:) :: A_K, A_C, A_F
-      double precision, allocatable, dimension(:,:) :: AK_time, u_pre, u_fut, rhs_time, u_init
-      integer, allocatable, dimension(:)            :: S_ipiv
-      character(len=20)                             :: solution_file_name
+      double precision, allocatable, dimension(:,:) :: AK_time, AK_LU, u_pre, u_fut, rhs_time, u_init
+      double precision, allocatable, dimension(:)   :: S_ferr, S_berr, S_work
+      integer, allocatable, dimension(:)            :: S_ipiv, S_iwork
+      !character(len=20)                             :: solution_file_name
       double precision :: delta_t
-      integer          :: time, info
+      integer          :: time, info, workdim, ans, i, j
       real             :: nt 
       
       
-      allocate( AK_time(ldAKban,ntotv))
+      allocate( AK_time(ldAKban,ntotv), AK_LU(ldAKban,ntotv))
       allocate( rhs_time(ntotv,1), u_init(ntotv,1) )
-      allocate(u_pre(S_ldSol, 1))
+      allocate( u_pre(S_ldSol, 1))
       allocate( u_fut(S_ldSol, 1))
       allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
       
+      allocate( S_work(workdim), S_iwork(S_ldSol), S_ferr(S_nrhs), S_berr(S_nrhs) )
       
-      delta_t  = ( time_fin - time_ini ) / (max_time + 1.0)    !Step size
-      time   = 0                                               !initializing the time
+      
+      S_ipiv   = 0
+      u_pre    = 0.0
+      u_fut    = 0.0
+      rhs_time = 0.0
+      AK_time  = 0.0
+      time     = 0                                            !initializing the time
+      delta_t  = ( time_fin - time_ini ) / (max_time + 1.0)   !Step size
+      
       !solution_file_name = 'CDR_u0000.post.res'
       write(*,*) ' '
       print*,'!============ TIME DISCRETIZATION ============!'
       write(*,"(A19,4X,F10.3,1X,A10)") ' - Initial time:          ', time_ini,' '
       write(*,"(A19,4X,F10.3,1X,A10)") ' - Final time:            ', time_fin,' '
-      write(*,"(A21,4X,F10.3,1X,A10)") ' - Step size(∆t):         ', delta_t,' '
-      write(*,"(A19,4X,F10.3,1X,A10)") ' - Number of steps:       ', max_time,' '
+      write(*,"(A19,8X,I3,1X,A10)")    ' - Number of steps:       ', max_time,' '
+      write(*,"(A21,7X,F10.6,1X,A10)") ' - Step size(∆t):         ', delta_t,' '
       write(*,*) ' '
-      call GID_PostProcess(u_pre, File_PostMsh, 'msh', time )
+      call GID_PostProcess(u_pre, File_PostMsh, 'msh', time, 0.0, time_fin)
       
       !call initial_condition(node_num, node_xy, u0_cond, u_init) 
       u_init = u0_cond                                  !Para prueba lo dejo sin subroutina
       u_pre  = u_init                                   !u in present time 
       
       write(*,*) ' '
-      print'(A11,I4,A2,F10.3,A)',' time step:',time,' = ',time_ini,' is the value of u by the initial condiction'
-      call GID_PostProcess(u_pre, File_PostRes, 'res', time)
+      print'(A11,I3,A3,F8.3,A)',' time step:',time,'  = ',time_ini,' is the value of u by the initial condiction'
+      call GID_PostProcess(u_pre, File_PostRes, 'res', time, 0.0, time_fin)
       print*, 'Starting time integration. . . . .'
       write(*,*) ' '
-      do nt = time_ini+delta_t,time_fin,delta_t
-        time = time + 1
+      nt = 0.0 
+      do time = 1, max_time +1
+        nt = nt + delta_t!,time_fin,delta_t
         
         call GlobalSystem_Time(N, dN_dxi, dN_deta, Hesxieta, S_ldsol, delta_t, u_pre, A_K, A_C, A_F)
-        
         !-------- Implicit Scheme --------!
         AK_time  = (1.0/delta_t)*A_C + A_K
         !AK_time  = A_C + delta_t*A_K
         rhs_time = A_F 
         call ApplyBVs(nofix,ifpre,presc,AK_time,rhs_time)
-        u_fut  = rhs_time                               !here mkl will rewrite u_fut by the solution vector
         
-        !------- Factorizing Matrix -------!
-        call dgbtrf(S_m, S_n, lowban, upban, AK_time, ldAKban, S_ipiv, info)
-        if(info.ne.0)then
-          call MKLfactoResult('dgbtrf',info)   !Aqui agregar el tiempo para en cada tiempo indicar el info de ejecucion
-          print'(A32,I3)', '<<<Factorization error in time: ', time
-        endif
-        !----- Solving System of Eqns -----!
-        call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AK_time, ldAKban, S_ipiv, u_fut, S_ldSol, info )
-        if(info.ne.0)then
-          call MKLsolverResult('dgbtrs',info)  !Aqui agregar el tiempo para en cada tiempo indicar el info de ejecucion
-          print'(A32,I3)', '<<<Solving System error in time: ', time
-        endif
+        ! print*, 'Global Band Matrix after BV'
+        ! write(*,"(I2,1x, F11.5)") (i, (AK_time(i,j), j=1,ntotv), i=1,ldAKban)
+        ! print*," "
+        ! print*, 'Global vetor after BV'
+        ! do i =1, ntotv
+        !   print '(F13.5)', rhs_time(i,1)
+        ! end do
         
+        !------------- Solver -------------!
+        u_fut = rhs_time   !here mkl will rewrite u_fut by the solution vector
+        AK_LU = AK_time  
+        !--- Factorizing Matrix
+        call dgbtrf(S_m, S_n, lowban, upban, AK_LU, ldAKban, S_ipiv, info)
+        if(info.ne.0)then
+          call MKLfactoResult('dgbtrf',info) 
+          print'(A32,I3)', '<<<Error in factorization at time: ', time
+        endif
+        !--- Solving System of Eqns
+        call dgbtrs(S_trans, S_n, lowban, upban, S_nrhs, AK_LU, ldAKban, S_ipiv, u_fut, S_ldSol, info )
+        if(info.ne.0)then
+          call MKLsolverResult('dgbtrs',info) 
+          print'(A32,I3)', '<<<Error in solving system of equation at time: ', time
+        endif
         u_pre = u_fut
-        
         !---------- Printing and writing results -----------!
-        !write(*,*) ' '
         !call file_name_inc(solution_file_name)
-        print'(A11,I4,A2,F10.3,A3,F10.3,A)',' time step:',time,' = ',nt,' of ',time_fin,' seg'
-        call GID_PostProcess(u_pre, File_PostRes, 'res', time)
+        print'(A11,I3,A3,F8.3,A5,F8.3,A5)',' time step:',time,' =',nt,'   of',time_fin,' seg'
+        call GID_PostProcess(u_pre, File_PostRes, 'res', time, nt, time_fin)
+        
         
       end do
       
+      print*, ' '
+      print*, 'Shape of Global K: ',shape(AK_time)
+      print*, 'Shape of Global F: ',shape(rhs_time)
+      print*, 'Shape of Solution: ',shape(u_pre)
+      write(*,*)
       DEALLOCATE( AK_time, rhs_time, u_pre, u_fut)
       
     end subroutine BackwardEuler
@@ -131,3 +153,33 @@ module timeInt
   !end contains
 
 end module timeInt
+
+!Estas lineas son para revisar la solucion e implementar el refinamiento de la solucion,
+!Deben agregarse despues de u_pre = u_fut
+        ! print*, ' '
+        ! print*,"Solution vector"
+        ! do i =1, ntotv
+        !   print '(F13.5)', u_fut(i,1)
+        ! end do
+        ! !----- Refining Solution -----!
+        ! call dgbrfs(S_trans, S_n, lowban, upban, S_nrhs, A_K, ldAKban, AK_LU, ldAKban, S_ipiv,&
+        !&            rhs_time, S_ldSol, u_fut, S_ldSol, S_ferr, S_berr, S_work, S_iwork, info )
+        ! if(info.ne.0)then
+        !   call MKLsolverResult('dgbrfs',info) 
+        !   print'(A32,I3)', '<<<The parameter ', info, ' had an illegal value in time: ', time
+        ! endif
+        ! print*, ' '
+        ! print*, 'Refined Solution'
+        ! do i =1, ntotv
+        !   print '(9F13.5)', u_fut(i,1)
+        ! end do
+        ! 
+        ! print*, ' '
+        ! print*, 'Avanzar? 1=yes ; other stop'
+        ! read(*,*) ans
+        ! if(ans.eq.1)then
+        !   continue
+        ! else
+        !   stop
+        ! endif
+        
