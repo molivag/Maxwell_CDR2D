a83272fd53074942f47395e74a2dc1dce72ad933
Marco O.G <marco.oliva@upc.edu>
2022-04-11 19:41:53 +0200

    PARAM: se agreo a lectura desde input file los parametros de tiempo

--- mod_param.f90
+++ mod_param.f90
@@ -4,10 +4,10 @@ module param
 
   character(len=5)  :: ProbType
   character(len=14) :: ElemType
-  integer           :: nevab, ntotv
+  integer           :: nevab, ntotv, nBVs, nBVscol, nband, max_time
   integer           :: upban, lowban, totban, ldAKban !variables defined in GlobalSystem
-  integer           :: DimPr, nelem, nnodes, nne, ndofn, totGp, kstab, ktaum, maxband, nBVs, nBVscol, nband
-  real              :: hnatu, patau
+  integer           :: DimPr, nelem, nnodes, nne, ndofn, totGp, kstab, ktaum, maxband
+  real              :: hnatu, patau, time_ini, time_fin, u0_cond
   integer,          allocatable, dimension(:,:)     :: lnods
   real,             allocatable, dimension(:,:)     :: coord
   double precision, allocatable, dimension(:,:)     :: ngaus, weigp
@@ -17,11 +17,8 @@ module param
   double precision, allocatable, dimension(:,:)     :: reama !Tensor materials
   double precision, allocatable, dimension(:)       :: force !Force vector 
   
-  !character(len=20), parameter :: File_element  = 'lnods.dat'
-  !character(len=20), parameter :: File_coord    = 'coord.dat'
-  !character(len=20), parameter :: File_tensors  = 'tensors.dat'
-  character(len=19), parameter :: File_PostMsh  = 'CDR_time.post.msh'
-  character(len=19), parameter :: File_PostRes  = 'CDR_time.post.res'
+  character(len=19), parameter :: File_PostMsh  = 'CDR_test.post.msh'
+  character(len=19), parameter :: File_PostRes  = 'CDR_test.post.res'
   
   contains
     
@@ -37,15 +34,10 @@ module param
       integer :: i,j, stat, k, l
       character(len=80) :: msg
       character(len=*), parameter  :: fileplace = "./"
-      ! double precision, allocatable, dimension(:,:,:,:) :: difma
-      ! double precision, allocatable, dimension(:,:,:)   :: conma
-      ! double precision, allocatable, dimension(:,:)     :: reama 
-      ! double precision, allocatable, dimension(:)       :: force 
       open(5, file=fileplace//'inputCDR.dsc',status='old', action='read',IOSTAT=stat, IOMSG=msg)
       
-      ! read(5, 100)  ElemType, DimPr, nelem, nnodes, nne, ndofn, totGp, maxband, kstab, ktaum, patau, hnatu
       read(5, 100) ElemType, ProbType, DimPr, nelem, nnodes, nne, & 
-      ndofn, totGp, maxband, kstab, ktaum, patau, hnatu
+      ndofn, totGp, maxband, time_ini, time_fin,max_time,u0_cond, kstab, ktaum, patau, hnatu
 
       allocate(lnods(nelem,nne+1))
       allocate(coord(nnodes,Dimpr+1))
@@ -61,12 +53,10 @@ module param
       
       if(ndofn.eq.1)then
         read(5,101) difma(1,1,1,1), difma(1,1,1,2), difma(1,1,2,2)
-        ! read(5,101) difma(1,1,1,2)
-        ! read(5,101) difma(1,1,2,2)
-    
+       
         read(5,101) conma(1,1,1)
         read(5,101) conma(1,1,2)
-    
+        
         read(5,101) reama(1,1)
         
         read(5,105) force(1)
@@ -75,16 +65,16 @@ module param
         read(5,102) difma(1,1,1,1), difma(1,2,1,1), difma(2,1,1,1),difma(2,2,1,1)
         read(5,102) difma(1,1,1,2), difma(1,2,1,2), difma(2,1,1,2),difma(2,2,1,2)
         read(5,102) difma(1,1,2,2), difma(1,2,2,2), difma(2,1,2,2),difma(2,2,2,2)
-    
+        
         read(5,102) conma(1,1,1), conma(1,2,1), conma(2,1,1), conma(2,2,1)
         read(5,102) conma(1,1,2), conma(1,2,2), conma(2,1,2), conma(2,2,2)
-    
+        
         read(5,102) reama(1,1), reama(1,2), reama(2,1), reama(2,2)
         
         read(5,106) force(1), force(2)
         
       elseif(ndofn.eq.3)then
-    
+        
         read(5,103) &
         difma(1,1,1,1), difma(1,2,1,1), difma(1,3,1,1), &
         difma(2,1,1,1), difma(2,2,1,1), difma(2,3,1,1), &
@@ -113,9 +103,9 @@ module param
         reama(3,1), reama(3,2), reama(3,3)
         
        read(5,107) force(1), force(2), force(3)
-      
+       
       end if
-
+      
       do i=1,nelem
         read(5,*,iostat=stat,iomsg=msg) (lnods(i,j), j =1,nne+1)
         IF ( stat /= 0 )then
@@ -135,7 +125,7 @@ module param
      
       nevab = ndofn*nne   
       ntotv = ndofn*nnodes
-
+      
       if(ndofn.eq.1)then
         difma(1,1,2,1) = difma(1,1,1,2)
       elseif(ndofn.eq.2)then
@@ -151,43 +141,50 @@ module param
         difma(3,3,2,1) = difma(3,3,1,2)
       end if
       
-      100 format(7/ 11x, A14,/ ,11x, A5,/, 7(11x,I5,/), 2/, 2(11x,I5,/),11x,f7.2,/,11x,F7.2,2/)    
-      101 format(1/,e15.5,2/)!,3/,e9.2,3/,e9.2,3/,e9.2)!,/,e9.2,/,e9.2)
+      
+      !100 format(7/ 11x, A14,/ ,11x, A5,/, 7(11x,I5,/), 2/, 11x,f7.2,/, 11x,f7.2,/, 11x, I3,/ 11x, f7.2,&
+      !&         2/, 2(11x,I5,/),11x,f7.2,/,11x,F7.2,2/)    !esta linea es de input: stabilization
+      
+      100 format(7/ 11x, A14,/ ,11x, A5,/, 7(11x,I5,/), 2/, 2(11x,f7.2,/),11x,I3,/,11x,f7.2,/,&
+      &         2/, 2(11x,I5,/),2(11x,F7.2,/),/)    !esta linea es de input: stabilization
+      101 format(1/,e15.5,2/)
       102 format(1/,e15.5, e15.5,/, e15.5,e15.5,/)
       103 format(1/,3(e15.5))
-      105 format(1/,e15.5,2/) !e15.5, e15.5,/)
-      106 format(1/,e15.5,e15.5,2/) !e15.5, e15.5,/)
-      107 format(1/,e15.5,e15.5,e15.5,2/) !e15.5, e15.5,/)
-       print*, ' '
-       print*, 'Diffusion matrix'
-       do i = 1,dimPr
-         do j = 1,DimPr
-           print"(A,2I1)", 'k_',i,j
-           do k = 1,ndofn
-             print"(e10.3,1x,e10.3, 1x, e10.3)",( difma(k,l,i,j), l=1,ndofn)
-           end do
-           print*,' '
-         end do
-       end do
-       print*, ' '  
-       print*, 'Convection matrix'
-       do k = 1, DimPr
-         print"(A,2I1)",'A_',k
-         do i = 1, ndofn
-           write(*, "(e10.3,1x,e10.3, 1x, e10.3)")( conma(i,j,k) ,j=1, ndofn)
-         end do
-         print*,''
-       end do
-       print*,'Reaction'
-       do i=1,ndofn
-         write(*,"(e10.3,1x,e10.3, 1x, e10.3)" )( reama(i,j) ,j=1,ndofn)
-       end do
-       print*,'force'
-       do i =1, ndofn
-         print"(e10.3)", force(i)
-       end do
-       print*, ' '
+      105 format(1/,e15.5,2/)
+      106 format(1/,e15.5,e15.5,2/) 
+      107 format(1/,e15.5,e15.5,e15.5,2/) 
+      
+      ! print*, ' '
+      ! print*, 'Diffusion matrix'
+      ! do i = 1,dimPr
+      !   do j = 1,DimPr
+      !     print"(A,2I1)", 'k_',i,j
+      !     do k = 1,ndofn
+      !       print"(e10.3,1x,e10.3, 1x, e10.3)",( difma(k,l,i,j), l=1,ndofn)
+      !     end do
+      !     print*,' '
+      !   end do
+      ! end do
+      ! print*, ' '  
+      ! print*, 'Convection matrix'
+      ! do k = 1, DimPr
+      !   print"(A,2I1)",'A_',k
+      !   do i = 1, ndofn
+      !     write(*, "(e10.3,1x,e10.3, 1x, e10.3)")( conma(i,j,k) ,j=1, ndofn)
+      !   end do
+      !   print*,''
+      ! end do
+      ! print*,'Reaction'
+      ! do i=1,ndofn
+      !   write(*,"(e10.3,1x,e10.3, 1x, e10.3)" )( reama(i,j) ,j=1,ndofn)
+      ! end do
+      ! print*,'force'
+      ! do i =1, ndofn
+      !   print"(e10.3)", force(i)
+      ! end do
+      ! print*, ' '
     end subroutine inputData
+    
   !end contains
     
 end module param
