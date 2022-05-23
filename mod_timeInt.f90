a43daf28a28908401d74dad67197315d7a165b75
Marco O.G <marco.oliva@upc.edu>
2022-05-23 13:40:09 +0200

    TIME: LA condicion de frontera cambio de proyectarse a solo igualarse a, en este caso, X para el test de capa limite. Se incluyeron las condiciones de frontera como dummy variable por que la condicion inicila requiere tener las condiciones de forntera aplicadas. En la llamada a aply bound cond la A_K se pone como dummy por que solo se requiere cond de forntera para el vector global. Se comentaron las lineas de proyeccion de condicion inicial asi como las del test de transporte

--- mod_timeInt.f90
+++ mod_timeInt.f90
@@ -5,7 +5,7 @@ module timeInt
   contains
 
 
-    subroutine initialCondition( N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldb, delta_t, Uinit) 
+    subroutine initialCondition( presc, ifpre, nofix, N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldb, delta_t, Uinit) 
 
       !* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
       ! Routine to proyect the initial condition into the FE space:
@@ -25,88 +25,102 @@ module timeInt
       implicit none
 
       external                                      :: dgbtrf, dgbtrs, dgbrfs
-      double precision, dimension(nne,TotGp), intent(in) :: N, dN_dxi, dN_deta
-      character(len=1), intent(in)                  :: S_trans
-      integer, intent(in)                           :: S_m, S_n, S_nrhs, S_ldb
 
-      double precision, allocatable, dimension(:,:) :: A_C0, A_U0
+      character(len=1), intent(in)                       :: S_trans
+      double precision, dimension(nne,TotGp), intent(in) :: N, dN_dxi, dN_deta
+      double precision, dimension(ndofn,nBVs), intent(in):: presc
+      integer, dimension(ndofn,nBVs), intent(in)         :: ifpre
+      integer, dimension(nBVs), intent(in)               :: nofix
+      integer, intent(in)                                :: S_m, S_n, S_nrhs, S_ldb
+
+      double precision, allocatable, dimension(:,:) :: A_C0, A_U0, dummy    
       double precision, dimension(DimPr, dimPr)     :: Jaco
       integer, allocatable, dimension(:)            :: S_ipiv
       double precision, dimension(nevab, nevab)     :: c0e, u0e
       double precision, dimension(nne)              :: basis, ux_cond
       integer, dimension(nne)                       :: nodeIDmap
       real, dimension(nne,DimPr)                    :: element_nodes
-
-      integer                                       :: ielem, igaus, ibase, S_info
+      integer                                       :: ielem, igaus, ibase, ii
       double precision                              :: dvol, detJ, aa, bb, cc, x0, xcoor
-      double precision, intent(out)                              :: delta_t
+      double precision, intent(out)                 :: delta_t
+
       double precision, allocatable, dimension(:,:), intent(out) ::  Uinit
       !SE DEBERIA CAMBIAR LAS VARIABLES DE TIEMPO A VARIABLES  LOCALES PONIENDOLAS COMO SALIDA DUMMY VARIABLE
       ! EN EL CALL DE INPUT DATA PUES OSLO SE USAN EN DOS SUBRUTINAS Y NO TIENE CASO QUE ESTEN EN TODO EL CODIGO
 
-      allocate( Uinit(ntotv,1), A_U0(ntotv,1), A_C0(ldAKban,ntotv) )
+      !allocate( Uinit(ntotv,1),  A_U0(ntotv,1), A_C0(ldAKban,ntotv) ) 
+      
+      allocate( Uinit(ntotv,1), dummy(ldAKban,ntotv))
+      
       delta_t  = ( time_fin - time_ini ) / (max_time + 1.0)   !Step size
       write(*,*) ' '
       print*,'!============ TIME DISCRETIZATION ============!'
       write(*,"(A19,8X,I2,1X,A10)")    ' - Method Selected:       ', theta,' '
-      write(*,"(A19,4X,F10.3,1X,A10)") '  - Initial time:          ', time_ini,' '
-      write(*,"(A19,4X,F10.3,1X,A10)") ' - Final time:            ', time_fin,' '
-      write(*,"(A19,8X,I3,1X,A10)")    ' - Number of steps:       ', max_time,' '
+      write(*,"(A19,4X,F10.3,1X,A10)") ' - Initial time:          ', time_ini,' '
+      write(*,"(A19,4X,F10.3,1X,A10)") ' - Final time:          ', time_fin,' '
+      write(*,"(A19,8X,I5,1X,A10)")    ' - Number of steps:       ', max_time,' '
       write(*,"(A21,7X,F10.6,1X,A10)") ' - Step size(∆t):         ', delta_t,' '
       write(*,*) ' '
 
-      aa = 5.0/7.0
-      bb = (7.0* sqrt(2.0) ) / 300.0    
-      x0 = u0cond      
+      !aa = 5.0/7.0
+      !bb = (7.0* sqrt(2.0) ) / 300.0    
+      !x0 = u0cond      
 
-      A_C0 = 0.0; A_U0 = 0.0
-      do ielem = 1, nelem 
-        c0e=0.0;  u0e=0.0
-        call SetElementNodes(ielem, element_nodes, nodeIDmap)
+      ! A_C0 = 0.0; A_U0 = 0.0
+      ! do ielem = 1, nelem 
+      !   c0e=0.0;  u0e=0.0
+      !   call SetElementNodes(ielem, element_nodes, nodeIDmap)
         
-        do ibase = 1, nne
-          xcoor = element_nodes(ibase,1)
-          !print*, 'xcoor',ibase, ' ', xcoor
-          cc    = (  xcoor - x0 )
-          ux_cond(ibase) = aa* exp( -(cc/bb)**2 )
-        end do
-        
-        !do-loop: compute element capacity and stiffness matrix Ke Ce and element vector Fe
-        do igaus = 1, TotGp
-          Jaco = J2D(element_nodes, dN_dxi, dN_deta, igaus)
-          detJ = m22det(Jaco)
-          !Jinv = inv2x2(Jaco)
-          dvol = detJ *  weigp(igaus,1)
-          !hmaxi = elemSize(Jinv)
-          do ibase = 1, nne
-            basis(ibase) = N(ibase,igaus)
-          end do
+      !   do ibase = 1, nne
+      !     !xcoor = 
+      !     !print*, 'xcoor',ibase, ' ', xcoor
+      !     !cc    = (  xcoor - x0 )
+      !     ux_cond(ibase) = element_nodes(ibase,1)!xcoor!aa* exp( -(cc/bb)**2 )
+      !   end do
+
+      !   !do-loop: compute element capacity and stiffness matrix Ke Ce and element vector Fe
+      !   do igaus = 1, TotGp
+      !     Jaco = J2D(element_nodes, dN_dxi, dN_deta, igaus)
+      !     detJ = m22det(Jaco)
+      !     !Jinv = inv2x2(Jaco)
+      !     dvol = detJ *  weigp(igaus,1)
+      !     !hmaxi = elemSize(Jinv)
+      !     do ibase = 1, nne
+      !       basis(ibase) = N(ibase,igaus)
+      !     end do
           
-          call Galerkin_Init_Cond(dvol, basis, ux_cond, c0e, u0e) 
-          !call TauMat(hmaxi,tauma)
-          !call Stabilization(dvol, basis, dN_dxy, HesXY, tauma, c0e, u0e)
-          !                                             Se debe estabilizar?
-        end do
+      !     call Galerkin_Init_Cond(dvol, basis, ux_cond, c0e, u0e) 
+      !     !call TauMat(hmaxi,tauma)
+      !     !call Stabilization(dvol, basis, dN_dxy, HesXY, tauma, c0e, u0e)
+      !     !                                             Se debe estabilizar?
+      !   end do
         
-        call Assemb_Glob_Mat(nodeIDmap, C0e, A_C0)   !Assemble Global Capacity Matrix at t=0 
-        call Assemb_Glob_Vec(nodeIDmap, u0e, A_U0)   !Assemble Global initial condition vector
+      !   call Assemb_Glob_Mat(nodeIDmap, C0e, A_C0)   !Assemble Global Capacity Matrix at t=0 
+      !   call Assemb_Glob_Vec(nodeIDmap, u0e, A_U0)   !Assemble Global initial condition vector
 
+      ! end do
+  
+      ! call dgbtrf(S_m, S_n, lowban, upban, A_C0, ldAKban, S_ipiv, S_info)  
+      ! if(S_info.ne.0)then
+      !   print*, 'In Matrix Factorization of Initial Condition:'
+      !   call MKLfactoResult('dgbtrf',S_info) 
+      ! endif
+      ! Uinit = A_U0
+      ! print*, ' '  
+      ! call dgbtrs(S_trans, S_n, lowban, upban, S_nrhs, A_C0, ldAKban, S_ipiv, Uinit, S_ldb, S_info )
+      ! if(S_info.ne.0)then
+      !   print*, 'In System of Equations of Initial Condition :'
+      !   call MKLsolverResult( 'dgbtrs', S_info )
+      ! end if
+      
+      do ii = 1 , nnodes
+        Uinit(ii,1)  = coord(ii,2)!Uinit             !u in present time 
       end do
 
-      call dgbtrf(S_m, S_n, lowban, upban, A_C0, ldAKban, S_ipiv, S_info)  
-      if(S_info.ne.0)then
-        print*, 'In Matrix Factorization of Initial Condition:'
-        call MKLfactoResult('dgbtrf',S_info) 
-      endif
-      Uinit = A_U0
-      print*, ' '  
-      call dgbtrs(S_trans, S_n, lowban, upban, S_nrhs, A_C0, ldAKban, S_ipiv, Uinit, S_ldb, S_info )
-      if(S_info.ne.0)then
-        print*, 'In System of Equations of Initial Condition :'
-        call MKLsolverResult( 'dgbtrs', S_info )
-      end if
-      
-    
+      call ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
+      deallocate(dummy)
+      return
+
     end subroutine initialCondition
     
     
@@ -135,7 +149,7 @@ module timeInt
       real             :: nt 
 
 
-      allocate( AK_time(ldAKban,ntotv), AK_LU(ldAKban,ntotv))
+      allocate( AK_time(ldAKban,ntotv),  AK_LU(ldAKban,ntotv))
       allocate( rhs_time(ntotv,1), Uinit(ntotv,1) )
       allocate( Uprev(S_ldSol, 1))
       allocate( Unext(S_ldSol, 1))
@@ -145,8 +159,9 @@ module timeInt
 
       Uprev = 0.0;  Unext = 0.0;  rhs_time = 0.0;  AK_time  = 0.0;  time = 0 
 
-      call initialCondition( N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol, delta_t, Uinit) 
-      Uprev  = Uinit                                   !u in present time 
+      call initialCondition(presc, ifpre, nofix, N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol, delta_t, Uinit) 
+      Uprev = Uinit
+      
 
       write(*,*) ' '
       print'(A11,I3,A3,F8.3,A)',' time step:',time,'  = ',time_ini,' is the value of u by the initial condiction'
