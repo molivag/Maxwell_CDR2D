04ad33456dc5ced21acd15efc2d34ec4ee6d7e5f
Marco O.G <marco.oliva@upc.edu>
2023-01-31 17:10:29 +0100

    TIME: It was removed the information that will beprinted in terminal related to time discretization, those information was passed to library in GeneralInfo, the variables time_ini, time_fin and mas_time where eliminated from the dummy arguments in the call and subroutine due to are global variables which where defined in PARAM, also it was removed the variable time_fin from GID_PostProcess, the variable N wich contains the basis functions was renamed for basfun. In the select case theta, was rewited the options 1 to BDF1 and 3 for Crank-Nicholson

--- mod_timeInt.f90
+++ mod_timeInt.f90
@@ -4,10 +4,10 @@ module timeInt
   use library!, only: ApplyBVs, GlobalSystem_Time, file_name_inc, GID_PostProcess, MKLsolverResult, MKLfactoResult
 
   contains
-
-
-    subroutine initialCondition( presc, ifpre, nofix, N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldb, delta_t, Uinit) 
-
+    
+    
+    subroutine initialCondition(presc,ifpre, nofix, N, dN_dxi,dN_deta,S_m, S_n, S_trans, S_nrhs, S_ipiv,S_ldb,delta_t, Uinit) 
+      
       !* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
       ! Routine to proyect the initial condition into the FE space:
       ! 
@@ -22,18 +22,18 @@ module timeInt
       !     
       !     from: Johnson, C. (2009). Numerical Sol of PDE by the FEM. Dover, 149-150
       !* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
-
+     
       implicit none
-
+     
       external                                      :: dgbtrf, dgbtrs, dgbrfs
-
+     
       character(len=1), intent(in)                       :: S_trans
       double precision, dimension(nne,TotGp), intent(in) :: N, dN_dxi, dN_deta
       double precision, dimension(ndofn,nBVs), intent(in):: presc
       integer, dimension(ndofn,nBVs), intent(in)         :: ifpre
       integer, dimension(nBVs), intent(in)               :: nofix
       integer, intent(in)                                :: S_m, S_n, S_nrhs, S_ldb
-
+     
       double precision, allocatable, dimension(:,:) :: A_C0, A_U0, dummy    
       double precision, dimension(DimPr, dimPr)     :: Jaco
       integer, allocatable, dimension(:)            :: S_ipiv
@@ -44,25 +44,16 @@ module timeInt
       integer                                       :: ielem, igaus, ibase, ii
       double precision                              :: dvol, detJ, aa, bb, cc, x0, xcoor
       double precision, intent(out)                 :: delta_t
-
+     
       double precision, allocatable, dimension(:,:), intent(out) ::  Uinit
       !SE DEBERIA CAMBIAR LAS VARIABLES DE TIEMPO A VARIABLES  LOCALES PONIENDOLAS COMO SALIDA DUMMY VARIABLE
       ! EN EL CALL DE INPUT DATA PUES OSLO SE USAN EN DOS SUBRUTINAS Y NO TIENE CASO QUE ESTEN EN TODO EL CODIGO
-
+     
       !allocate( Uinit(ntotv,1),  A_U0(ntotv,1), A_C0(ldAKban,ntotv) ) 
       
       allocate( Uinit(ntotv,1), dummy(ldAKban,ntotv))
       
       delta_t  = ( time_fin - time_ini ) / (max_time + 1.0)   !Step size
-      write(*,*) ' '
-      print*,'!============ TIME DISCRETIZATION ============!'
-      write(*,"(A19,8X,I2,1X,A10)")    ' - Method Selected:       ', theta,' '
-      write(*,"(A19,4X,F10.3,1X,A10)") ' - Initial time:          ', time_ini,' '
-      write(*,"(A19,4X,F10.3,1X,A10)") ' - Final time:          ', time_fin,' '
-      write(*,"(A19,8X,I5,1X,A10)")    ' - Number of steps:       ', max_time,' '
-      write(*,"(A21,7X,F10.6,1X,A10)") ' - Step size(∆t):         ', delta_t,' '
-      write(*,*) ' '
-
       !aa = 5.0/7.0
       !bb = (7.0* sqrt(2.0) ) / 300.0    
       !x0 = u0cond      
@@ -125,21 +116,21 @@ module timeInt
     end subroutine initialCondition
     
     
-    subroutine TimeIntegration(N, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta,&
-    &                    time_ini, time_fin, max_time, nofix, ifpre, presc, &
-    &                    S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol)
+    subroutine TimeIntegration(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta,&
+    &                    nofix, ifpre, presc, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol)
+     
       implicit none
-
+      
       external :: dgbtrf, dgbtrs, dgbrfs
-
-      double precision, dimension(nne,TotGp), intent(in)     :: N, dN_dxi, dN_deta
-      double precision, dimension(nne,TotGp), intent(in)     :: hes_xixi, hes_xieta, hes_etaeta
-      double precision, dimension(ndofn,nBVs), intent(in)    :: presc
-      integer, dimension(ndofn,nBVs), intent(in)             :: ifpre
-      integer, dimension(nBVs), intent(in)                   :: nofix
-      character(len=1), intent(in)                           :: S_trans
-      integer,intent (in)                                    :: S_m, S_n, S_nrhs, S_ldSol, max_time
-      real, intent(in)                                       :: time_ini, time_fin
+      
+      double precision, dimension(nne,TotGp), intent(in)  :: basfun, dN_dxi, dN_deta
+      double precision, dimension(nne,TotGp), intent(in)  :: hes_xixi, hes_xieta, hes_etaeta
+      double precision, dimension(ndofn,nBVs),intent(in)  :: presc
+      integer, dimension(ndofn,nBVs), intent(in)          :: ifpre
+      integer, dimension(nBVs), intent(in)                :: nofix
+      character(len=1), intent(in)                        :: S_trans
+      integer,intent (in)                                 :: S_m, S_n, S_nrhs, S_ldSol!, max_time
+      !real, intent(in)                                    :: time_ini, time_fin
       ! - - Local Variables - -!
       double precision, allocatable, dimension(:,:) :: A_K, A_C, A_F, Ftime
       double precision, allocatable, dimension(:,:) :: AK_time, AK_LU, Uprev, Unext, rhs_time, Uinit
@@ -148,33 +139,36 @@ module timeInt
       double precision :: delta_t
       integer          :: time, info!, workdim
       real             :: nt 
-
-
+     
+     
       allocate( AK_time(ldAKban,ntotv),  AK_LU(ldAKban,ntotv))
       allocate( rhs_time(ntotv,1), Uinit(ntotv,1) )
       allocate( Uprev(S_ldSol, 1))
       allocate( Unext(S_ldSol, 1))
       allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
-
+     
       !allocate( S_work(workdim), S_iwork(S_ldSol), S_ferr(S_nrhs), S_berr(S_nrhs) )
-
-      Uprev = 0.0;  Unext = 0.0;  rhs_time = 0.0;  AK_time  = 0.0;  time = 0 
-
-      call initialCondition(presc, ifpre, nofix, N, dN_dxi, dN_deta, S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol, delta_t, Uinit) 
-      Uprev = Uinit
       
+      Uprev = 0.0;  Unext = 0.0;  rhs_time = 0.0;  AK_time  = 0.0;  time = 0 
+     
+      call initialCondition(presc,ifpre,nofix,basfun,dN_dxi,dN_deta,S_m,S_n,S_trans,S_nrhs,S_ipiv,S_ldSol,delta_t,Uinit) 
+      Uprev = Uinit
+     
       write(*,*) ' '
       print'(A11,I3,A3,F8.3,A)',' time step:',time,'  = ',time_ini,' is the value of u by the initial condiction'
-      call GID_PostProcess(Uprev, 'res', time, 0.0, time_fin)
+      call GID_PostProcess(Uprev, 'res', time, 0.0)
       print*, 'Starting time integration. . . . .'
       write(*,*) ' '
-
+     
       !the allocate of A_K and A_F are inside of GlobalSystem
-      call GlobalSystem(N, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
+      call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
       select case(theta)
-        case(2)!--- LHS BDF1
+        case(1)! BDF1
           AK_time  = (1.0/delta_t)*A_C + A_K 
-        case(3)!-- CN
+        case(2)! BDF2
+          print*, 'BDF2 not available yet '
+          stop
+        case(3)! Crank-Nicholson
           AK_time  = (1.0/delta_t)*A_C + 0.5*A_K !A_C + delta_t*A_K
       end select  
       
@@ -182,14 +176,16 @@ module timeInt
       nt = 0.0 
       do time = 1, max_time +1
         nt = nt + delta_t!,time_fin,delta_t
-
-        call TimeContribution(N, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, Uprev, Ftime)
+       
+        call TimeContribution(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, Uprev, Ftime)
         !--- LHS
         rhs_time = Ftime  
         call ApplyBVs(nofix,ifpre,presc,AK_time,rhs_time)
-        !------------- Solver -------------!
+        
+        !------------- solver -------------!
         Unext = rhs_time   !here mkl will rewrite Unext by the solution vector
         AK_LU = AK_time  
+        
         !--- Factorizing Matrix
         call dgbtrf(S_m, S_n, lowban, upban, AK_time, ldAKban, S_ipiv, info)
         if(info.ne.0)then
@@ -202,22 +198,24 @@ module timeInt
           call MKLsolverResult('dgbtrs',info) 
           print'(A32,I3)', '<<<Error in solving system of equation at time: ', time
         endif
+        !----------- end solver ------------!
+        
         !--- Update Time
         Uprev = Unext
         !---------- Printing and writing results -----------!
-        print'(A11,I3,A3,F8.3,A5,F8.3,A5)',' time step:',time,' =',nt,'   of',time_fin,' seg'
-        call GID_PostProcess(Uprev, 'res', time, nt, time_fin)
-
+        print'(A11,I3,A3,F10.5,A5,F10.5,A5)',' time step:',time,' =',nt,' of',time_fin,' seg'
+        call GID_PostProcess(Uprev, 'res', time, nt)
+        
       end do
-
-      print*, ' '
-      print*, 'Shape of Global K: ',shape(AK_time)
-      print*, 'Shape of Global F: ',shape(rhs_time)
-      print*, 'Shape of Solution: ',shape(Uprev)
-      write(*,*)
-      call GID_PostProcess(Uprev,'msh', time, 0.0, time_fin) 
+      
+      !print*, ' '
+      !print*, 'Shape of Global K: ',shape(AK_time)
+      !print*, 'Shape of Global F: ',shape(rhs_time)
+      !print*, 'Shape of Solution: ',shape(Uprev)
+      !write(*,*)
+      call GID_PostProcess(Uprev,'msh', time, 0.0) 
+      print"(1x, A26,A30)", File_PostProcess//'.post.res', 'written succesfully in Pos/ '
       DEALLOCATE( AK_time, rhs_time, Uprev, Unext)
-
     end subroutine TimeIntegration
     
     
