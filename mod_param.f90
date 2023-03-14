7947b1f2b60d2f45e5b803c237a3c21e9cdcf3c4
Marco O.G <marco.oliva@upc.edu>
2023-03-14 11:51:51 +0100

    PARAM: Se incremento el largo de TestNo para agregar una pequeña descripcion, se agrego elemsour que contiene el numeor de elementos que contienen la fuente y skipline el numero de lineas a saltar antes de empezar a leer la malla, la variable mu cambio por lambda como en el papper, se agrego el vector sourceLocation que contien los elementos de la fuente, se agrego un loop para leer el numero de elementos que contiene la fuente segun la variable elemSour, se agrego el caso de if kstab=6-->MVAF y tomara los parametros de estabilizacion, se agrego el formato segun corresponde para el nuevo archivo de entrada con estas modificaciones

--- mod_param.f90
+++ mod_param.f90
@@ -3,21 +3,23 @@ module param
   implicit none
 
   character(len=12) :: File_PostProcess, error_name, coord_name, conec_name
+  character(len=14) :: testNo
   character(len=4)  :: InitElemType, ProbType
-  character(len=2)  :: refiType, testNo
+  character(len=2)  :: refiType
   integer           :: initnevab, initntotv, nBVs, nBVscol, nband, max_time, simul
   integer           :: upban, lowban, totban, ldAKban !variables defined in GlobalSystem
   integer           :: initElem, initNodes, DimPr, nne, ndofn, totGp, kstab, ktaum, maxband, theta
-  integer           :: x_0, y_0, x_1, y_1
+  integer           :: elemSour, skipline
   real              :: hnatu, patau, time_ini, time_fin, u0cond
-  real              :: Cu,mu, ell, helem, i_exp, n_val
+  real              :: Cu,lambda, ell, helem, i_exp, n_val
   double precision, allocatable, dimension(:,:)     :: ngaus, weigp
 
   double precision, allocatable, dimension(:,:,:,:) :: difma
   double precision, allocatable, dimension(:,:,:)   :: conma
   double precision, allocatable, dimension(:,:)     :: reama !Tensor materials
   double precision, allocatable, dimension(:)       :: force !Force vector 
-  
+  integer         , allocatable, dimension(:)       :: sourceLocation 
+
   contains
     
     subroutine inputData
@@ -32,14 +34,14 @@ module param
       double precision  :: param_stab1, param_stab2 
       character(len=180)            :: msg
       character(len=*), parameter   :: fileplace = "./"
-      integer                       ::  stat
+      integer                       ::  stat, ii
       
       open(5, file=fileplace//'inputCDR.dsc',status='old', action='read',IOSTAT=stat, IOMSG=msg)
       
-      read(5, 100,iostat=stat,iomsg=msg) InitElemType,ProbType,DimPr,ndofn,totGp,simul,&
-      initElem, initNodes, nne, refiType, theta, time_ini, time_fin,max_time, u0cond,&
-      kstab, ktaum, patau, hnatu, Cu, ell, i_exp, n_val, mu, testNo, File_PostProcess,&
-      error_name, coord_name, conec_name, x_0, y_0, x_1, y_1
+      read(5, 100,iostat=stat,iomsg=msg) InitElemType,ProbType,DimPr,ndofn,totGp,simul,elemSour,skipline,&
+      initElem, initNodes, nne, hnatu, i_exp, refiType, theta, time_ini, time_fin, max_time, u0cond,&
+      kstab, ktaum, patau, n_val, Cu, ell, lambda, testNo, File_PostProcess,&
+      error_name, coord_name, conec_name
       
       if (stat.ne.0) then
         print*, ' '
@@ -56,18 +58,12 @@ module param
       allocate( conma(ndofn,ndofn,DimPr) )
       allocate( reama(ndofn,ndofn) )
       allocate( force(ndofn) )
+      allocate( sourceLocation(elemSour) )
       
       difma = 0.0
       conma = 0.0
       reama = 0.0
       force = 0.0
-      
-      helem = 2**(-i_exp)
-     
-      !Parameter of stabilization in augmented formulation
-      param_stab1 = Cu*mu*(helem**2/ell**2)
-      !print*, param_stab1
-      param_stab2 = ell**2 / mu
       
       if(ndofn.eq.1)then
         read(5,101) difma(1,1,1,1), difma(1,1,1,2)
@@ -138,35 +134,48 @@ module param
         if (stat.ne.0)print'(A8,1x,A180)','iomsg = ',msg
         
        read(5,107,iostat=stat,iomsg=msg) force(1), force(2), force(3)
-        !if (stat.ne.0)print'(A8,1x,A180)','force iomsg = ',msg
-       
-        difma(1,1,1,1) = difma(1,1,1,1)*param_stab1
-        difma(2,2,1,1) = difma(2,2,1,1)*mu
-        difma(3,3,1,1) = difma(3,3,1,1)*param_stab2
-        
-        difma(1,2,1,2) = difma(1,2,1,2)*param_stab1
-        difma(1,2,1,2) = difma(1,2,1,2)*mu
-        
-        difma(2,1,2,1) = difma(2,1,2,1)*mu
-        difma(2,1,2,1) = difma(2,1,2,1)*param_stab1
-        
-        difma(1,1,2,2) = difma(1,1,2,2)*mu
-        difma(2,2,2,2) = difma(2,2,2,2)*param_stab1
-        difma(3,3,2,2) = difma(3,3,2,2)*param_stab2
+        if (stat.ne.0)print'(A8,1x,A180)','force iomsg = ',msg
         
       end if
      
+      do ii =1, elemSour
+        read(5,*,iostat=stat,iomsg=msg) sourceLocation(ii)
+      end do
+      
       close(5)
+      
+      
+      if(kstab.eq.6)then
+        helem = 2**(-i_exp) 
+        !Parameter of stabilization in augmented formulation
+        param_stab1 = Cu*lambda*(helem**2/ell**2)
+        !print*, param_stab1
+        param_stab2 = ell**2 / lambda
+        
+        difma(1,1,1,1) = difma(1,1,1,1)*param_stab1
+        difma(2,2,1,1) = difma(2,2,1,1)*lambda
+        difma(3,3,1,1) = difma(3,3,1,1)*param_stab2
+        
+        difma(1,2,1,2) = difma(1,2,1,2)*param_stab1
+        difma(2,1,1,2) = difma(2,1,1,2)*lambda
+        
+        difma(1,2,2,1) = difma(1,2,2,1)*lambda
+        difma(2,1,2,1) = difma(2,1,2,1)*param_stab1
+        
+        difma(1,1,2,2) = difma(1,1,2,2)*lambda
+        difma(2,2,2,2) = difma(2,2,2,2)*param_stab1
+        difma(3,3,2,2) = difma(3,3,2,2)*param_stab2
+      end if
       
       !Initial elemental and global variables, it will changes if refination is selected.
       initnevab = ndofn*nne
       initntotv = ndofn*initNodes
       
-      100 format(7/ 11x, A4,/ ,11x, A4,/, 4(11x,I5,/), 2/,&                      !model parameters
-      &          3(11x,I7,/), 11x,A2,/      ,2/,&                                !geometry
-      &          11x,I1,/, 2(11x,f7.2,/),11x,I7,/,11x,f7.2,/, 2/,&               !time
-      &          2(11x,I5,/), 6(11x,F7.2,/), 1(11x,e15.5,/),  2/,& !stabi
-      &          11x,A2,/, 4(11x,A12,/), 2/, 4(11x,I7,/), / )                     !source location
+      100 format(7/ 11x, A4,/ ,11x, A4,/, 6(11x,I5,/), 2/        ,&  !model parameters
+      &          3(11x,I7,/), 2(11x,F7.2,/), 11x,A2,/, 2/        ,&  !geometry
+      &          11x,I1,/, 2(11x,f7.2,/), 11x,I7,/,11x,f7.2,/, 2/,&  !time
+      &          2(11x,I5,/), 4(11x,F7.2,/), 1(11x,e15.5,/),   2/,&  !stabi
+      &          11x,A14,/, 4(11x,A12,/), / )                         !output files
      
       101 format(1/,F12.5,2/)
       102 format(1/,e15.5, e15.5,/, e15.5,e15.5,/)
