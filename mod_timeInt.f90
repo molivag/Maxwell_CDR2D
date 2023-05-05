adbd726a39bd328043dbedc2e7a24b80df21cea5
Marco O.G <marco.oliva@upc.edu>
2023-05-05 15:21:16 +0200

    TIME: From initialCondition, was removed the input variables leving them just the necesary to comput the boundary conditions, it was removed all the lines to build the projection for initial condition vector. We can find the lines in the last commit of TIME. The name of the subroutine TimeContribution change by Time_RHS, in the TimeIntegration subroutine it was changed the call for initialCondition. Moreover, the lines to comput AK_Time, was moved inside Time Stepping loop. Even though it is computed just once, if I put it out of the loop the solution EXPLODE, I need to check it. Finally the AK_time was re-initialized at the end of time-stepping just before to start another step.

--- mod_timeInt.f90
+++ mod_timeInt.f90
@@ -7,7 +7,8 @@ module timeInt
     !
     != = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     !
-    subroutine initialCondition(presc,ifpre, nofix, N, dN_dxi,dN_deta,S_m, S_n, S_trans, S_nrhs, S_ipiv,S_ldb,delta_t, Uinit) 
+    !subroutine initialCondition(presc,ifpre, nofix, N, dN_dxi,dN_deta,S_m, S_n, S_trans, S_nrhs, S_ipiv,S_ldb,delta_t, Uinit) 
+    subroutine initialCondition(presc,ifpre, nofix, delta_t, Uinit) 
       
       !* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
       ! Routine to proyect the initial condition into the FE space:
@@ -26,101 +27,29 @@ module timeInt
      
       implicit none
      
-      external                                      :: dgbtrf, dgbtrs, dgbrfs
+      !external                                      :: dgbtrf, dgbtrs, dgbrfs
      
-      character(len=1), intent(in)                       :: S_trans
-      double precision, dimension(nne,TotGp), intent(in) :: N, dN_dxi, dN_deta
       double precision, dimension(ndofn,nBVs), intent(in):: presc
       integer, dimension(ndofn,nBVs), intent(in)         :: ifpre
       integer, dimension(nBVs), intent(in)               :: nofix
-      integer, intent(in)                                :: S_m, S_n, S_nrhs, S_ldb
-     
-      double precision, allocatable, dimension(:,:) :: A_C0, A_U0, dummy    
-      double precision, dimension(DimPr, dimPr)     :: Jaco
-      integer, allocatable, dimension(:)            :: S_ipiv
-      double precision, dimension(nevab, nevab)     :: c0e, u0e
-      double precision, dimension(nne)              :: basis, ux_cond
-      integer, dimension(nne)                       :: nodeIDmap
-      real, dimension(nne,DimPr)                    :: element_nodes
-      integer                                       :: ielem, igaus, ibase, ii
-      double precision                              :: dvol, detJ, aa, bb, cc, x0, xcoor
-      double precision, intent(out)                 :: delta_t
-     
+      double precision, allocatable, dimension(:,:) :: dummy
       double precision, allocatable, dimension(:,:), intent(out) ::  Uinit
-      !SE DEBERIA CAMBIAR LAS VARIABLES DE TIEMPO A VARIABLES 
-      !LOCALES PONIENDOLAS COMO SALIDA DUMMY VARIABLE EN EL CALL DE INPUT DATA
-      ! PUES OSLO SE USAN EN DOS SUBRUTINAS Y NO TIENE CASO QUE ESTEN EN TOD0 EL CODIGO
-     
-      !allocate( Uinit(ntotv,1),  A_U0(ntotv,1), A_C0(ldAKban,ntotv) ) 
+      double precision, intent(out)                 :: delta_t
+      
       
       allocate( Uinit(ntotv,1), dummy(ldAKban,ntotv))
       
       delta_t  = ( time_fin - time_ini ) / (max_time + 1.0)   !Step size
-      !aa = 5.0/7.0
-      !bb = (7.0* sqrt(2.0) ) / 300.0    
-      !x0 = u0cond      
-
-      ! A_C0 = 0.0; A_U0 = 0.0
-      ! do ielem = 1, nelem 
-      !   c0e=0.0;  u0e=0.0
-      !   call SetElementNodes(ielem, element_nodes, nodeIDmap)
-        
-      !   do ibase = 1, nne
-      !     !xcoor = 
-      !     !print*, 'xcoor',ibase, ' ', xcoor
-      !     !cc    = (  xcoor - x0 )
-      !     ux_cond(ibase) = element_nodes(ibase,1)!xcoor!aa* exp( -(cc/bb)**2 )
-      !   end do
-
-      !   !do-loop: compute element capacity and stiffness matrix Ke Ce and element vector Fe
-      !   do igaus = 1, TotGp
-      !     Jaco = J2D(element_nodes, dN_dxi, dN_deta, igaus)
-      !     detJ = m22det(Jaco)
-      !     !Jinv = inv2x2(Jaco)
-      !     dvol = detJ *  weigp(igaus,1)
-      !     !hmaxi = elemSize(Jinv)
-      !     do ibase = 1, nne
-      !       basis(ibase) = N(ibase,igaus)
-      !     end do
-          
-      !     call Galerkin_Init_Cond(dvol, basis, ux_cond, c0e, u0e) 
-      !     !call TauMat(hmaxi,tauma)
-      !     !call Stabilization(dvol, basis, dN_dxy, HesXY, tauma, c0e, u0e)
-      !     !                                             Se debe estabilizar?
-      !   end do
-        
-      !   call Assemb_Glob_Mat(nodeIDmap, C0e, A_C0)   !Assemble Global Capacity Matrix at t=0 
-      !   call Assemb_Glob_Vec(nodeIDmap, u0e, A_U0)   !Assemble Global initial condition vector
-
-      ! end do
-  
-      ! call dgbtrf(S_m, S_n, lowban, upban, A_C0, ldAKban, S_ipiv, S_info)  
-      ! if(S_info.ne.0)then
-      !   print*, 'In Matrix Factorization of Initial Condition:'
-      !   call MKLfactoResult('dgbtrf',S_info) 
-      ! endif
-      ! Uinit = A_U0
-      ! print*, ' '  
-      ! call dgbtrs(S_trans, S_n, lowban, upban, S_nrhs, A_C0, ldAKban, S_ipiv, Uinit, S_ldb, S_info )
-      ! if(S_info.ne.0)then
-      !   print*, 'In System of Equations of Initial Condition :'
-      !   call MKLsolverResult( 'dgbtrs', S_info )
-      ! end if
       
-      
-      !do ii = 1 , nnodes
-      !  Uinit(ii,1)  = coord(ii,1)!Uinit             !u in present time for COnvection diffusion test.
-      !end do
-
       call ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
       deallocate(dummy)
       return
-
+      
     end subroutine initialCondition
     !
     != = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     !
-    subroutine TimeContribution(N, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, ugl_pre, A_F)
+    subroutine Time_RHS(N, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, ugl_pre, A_F)
       use sourceTerm
       
       implicit none
@@ -186,7 +115,7 @@ module timeInt
         
       end do
       
-    end subroutine TimeContribution
+    end subroutine Time_RHS
     !
     != = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     !
@@ -225,7 +154,8 @@ module timeInt
       
       Uprev = 0.0;  Unext = 0.0;  rhs_time = 0.0;  AK_time  = 0.0;  time = 0 
      
-      call initialCondition(presc,ifpre,nofix,basfun,dN_dxi,dN_deta,S_m,S_n,S_trans,S_nrhs,S_ipiv,S_ldSol,delta_t,Uinit) 
+      call initialCondition(presc,ifpre, nofix, delta_t, Uinit) 
+      !call initialCondition(presc,ifpre,nofix,basfun,dN_dxi,dN_deta,S_m,S_n,S_trans,S_nrhs,S_ipiv,S_ldSol,delta_t,Uinit) 
       Uprev = Uinit
      
       write(*,*) ' '
@@ -235,23 +165,35 @@ module timeInt
       write(*,*) ' '
      
       !the allocate of A_K and A_F are inside of GlobalSystem
-      call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
-      select case(theta)
-        case(1)! BDF1
-          AK_time  = (1.0/delta_t)*A_C + A_K 
-        case(2)! BDF2
-          print*, 'BDF2 not available yet '
-          stop
-        case(3)! Crank-Nicholson
-          AK_time  = (1.0/delta_t)*A_C + 0.5*A_K !A_C + delta_t*A_K
-      end select  
+      !call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
+      !select case(theta)
+      !  case(1)! BDF1
+      !    AK_time  = (1.0/delta_t)*A_C + A_K 
+      !  case(2)! BDF2
+      !    print*, 'BDF2 not available yet '
+      !    stop
+      !  case(3)! Crank-Nicholson
+      !    AK_time  = (1.0/delta_t)*A_C + 0.5*A_K !A_C + delta_t*A_K
+      !end select  
       
       !-- Time Stepping
       nt = 0.0 
       do time = 1, max_time +1
         nt = nt + delta_t!,time_fin,delta_t
        
-        call TimeContribution(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, Uprev, Ftime)
+        call Time_RHS(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, delta_t, Uprev, Ftime)
+        call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
+        deallocate(A_F)
+        select case(theta)
+          case(1)! BDF1
+            AK_time  = (1.0/delta_t)*A_C + A_K 
+          case(2)! BDF2
+            print*, 'BDF2 not available yet '
+            stop
+          case(3)! Crank-Nicholson
+            AK_time  = (1.0/delta_t)*A_C + 0.5*A_K !A_C + delta_t*A_K
+        end select  
+        
         !--- LHS
         rhs_time = Ftime  
         call ApplyBVs(nofix,ifpre,presc,AK_time,rhs_time)
@@ -276,9 +218,12 @@ module timeInt
         
         !--- Update Time
         Uprev = Unext
+        AK_time = 0.0
         !---------- Printing and writing results -----------!
-        print'(A11,I3,A3,F10.5,A5,F10.5,A5)',' time step:',time,' =',nt,' of',time_fin,' seg'
+        print'(A12,I0,A,F10.5,A5,F10.5,A5)',' time step: ',time,' =',nt,' of',time_fin,' seg'
         call GID_Time_Results(Uprev, 'res', time, nt)
+        
+        !if(time.eq.2)stop
         
       end do
       
