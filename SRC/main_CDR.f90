927e910f8202c7379f2dfc0b0e3f2700518dcfcc
Marco Oliva <maoliva@cimne.upc.edu>
2023-12-08 15:18:19 +0100

    MAIN: Se agrego un select case para el caso transitorio TIME o estatico STAT, esta variable global se lee desde el input como antes pero ahora el main se ha optimizado y hasta las condiciones de forntera el codigo es general a ambos casos luego el caclulo de las matrices globales M y K y el vector global F se hace en cada caso, con esto se evita calcular GlobalSystem antes de iniciar el tiempo. Tambien se optimizo el calculo de CPU poniendo la hora y fecha de finalizacion mediante date y la transformacion a minutos es (CPU_time/30)/10

--- SRC/main_CDR.f90
+++ SRC/main_CDR.f90
@@ -4,6 +4,7 @@ program main_CDR3d
 
 implicit none
 
+  external :: fdate
   ! - - - - - - - - - - * * * Variable declaration * * * * * * * - - - - - - - - - -!
   character(len=19)                                 :: name_inputFile
   character(len=13)                                 :: geometry_File
@@ -14,7 +15,7 @@ implicit none
   double precision, allocatable, dimension(:,:)     :: Bvs
   integer,          allocatable, dimension(:,:)     :: condition, ifpre
   integer,          allocatable, dimension(:)       :: nofix
-  real                                              :: start, finish
+  double precision                                              :: start, finish
   ! - - - - - - - - * * * Variable declaration (SOLVER) * * * * * * * - - - - - - - !
   double precision, allocatable, dimension(:,:,:)   :: grad_u_sol
   double precision, allocatable, dimension(:,:)     :: AK_LU, u_sol
@@ -23,16 +24,14 @@ implicit none
   !double precision, allocatable, dimension(:)      :: S_ferr, S_berr, S_work
   integer,          allocatable, dimension(:)       :: S_ipiv!, S_iwork
   character(len=1)                                  :: S_trans
-  integer                                           :: S_m, S_n, S_nrhs, info, S_ldSol,workdim, ii
+      character(len=24)           :: date
+  integer                                           :: S_m, S_n, S_nrhs, info, S_ldSol, ii
   
   !-----Input File
-  !name_inputFile = 'TesisDCinputCDR.dsc'
-  !name_inputFile = 'Maxwel_inputCDR.dsc' 
+  call cpu_time(start)
   name_inputFile = 'tMaxwelinputCDR.dsc' 
-  !name_inputFile = 'Stokes_InputCDR.dsc' 
   
   !--------------- Input Data ---------------!
-  call cpu_time(start)
   call inputData(name_inputFile, geometry_file)
   
   !--------------- Geometry -----------------!
@@ -46,113 +45,110 @@ implicit none
   !------- Computing half bandwidth  --------!
   call BandWidth
 
-  !---------- Global Matrix and Vector ------!
-  print*,'  •BUILDING GLOBAL MATRIX (K) AND VECTOR (F).....'
-  call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
-  !the allocate of A_K and A_F are inside of GlobalSystem
-
   !------- Setting Boundary Conditions ------!
-  call SetBoundVal( nBVs, nBVscol) !Esta funcion crea el archivo BVs.dat
-  allocate( condition(nBvs,nBVscol-ndofn), BVs(nBVs,ndofn) ) !Designo la memoria para la matriz de nodos con valor en la frontera
+  !Esta funcion crea el archivo BVs.dat
+  call SetBoundVal( nBVs, nBVscol)
+  !Designo memoria para la matriz de nodos con valor en la frontera
+  allocate( condition(nBvs,nBVscol-ndofn), BVs(nBVs,ndofn) ) 
   call ReadFile(nBVs, nBVscol, condition, BVs)!Reading the boundary values file and store it at BVs
   !memoria para el numero de nodo, si esta preescrito o no y su valor correspondiente
   allocate( nofix(nBVs), ifpre(ndofn,nBVs), presc(ndofn,nBVs) )
   call VinculBVs( condition,  BVs, nofix, ifpre, presc )
 
-  !----- Setting MKL-Solver Parammeters -----!
-  S_m     = size(A_K,2)  !antes ntotv
-  S_n     = size(A_K,2)  !antes ntotv
-  S_ldSol = max(1,S_n)
-  S_trans = 'N'
-  S_nrhs  = 1
-
   !-------- Problem Type Definition --------!
-  if(ProbType .eq. 'TIME')then !transient case
-    
-    print*, ' '
-    print*, "!=============== Time Integration =============! "
-     !call TimeIntegration(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta,&
-    !&                    nofix, ifpre, presc,S_m, S_n, S_trans, S_nrhs, S_ipiv, S_ldSol)
-    call Timeintegration(basfun, dN_dxi, dN_deta,hes_xixi,hes_xieta,hes_etaeta,&
-      & nofix, ifpre, prescl, Ex_field)
+  select case(ProbType)
+    case('TIME')
+      
+      print*, ' '
+      print*, "!=============== Time Integration =============! "
+      call Timeintegration(basfun, dN_dxi, dN_deta,hes_xixi,hes_xieta,hes_etaeta,&
+      &                    nofix, ifpre, presc, Ex_field)
+     
+      ! call Res_Matlab(Ex_field)
+     
+      !---------- Memory Relase -----------!
+      deallocate( basfun, dN_dxi, dN_deta, BVs, nofix, ifpre, presc)
+      deallocate(hes_xixi, hes_xieta, hes_etaeta) 
+    case('STAT')
+      !---------- Global Matrix and Vector ------!
+      print*,'  •BUILDING GLOBAL MATRIX (K) AND VECTOR (F).....'
+      !the allocate of A_K and A_F are inside of GlobalSystem
+      call GlobalSystem(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, A_C, A_K, A_F)
+      call ApplyBVs(nofix,ifpre,presc,A_K, A_F)
+      
+      !Applying the source term for DC simulation  j = I*δ(x-x0)δ(y-y0)
+      if(BCsProb.eq.5)then
+        continue
+      else
+        if(((ndofn.eq.1).or.(ndofn.eq.3)).and.((exacSol.eq.5).or.(exacSol.eq.2)))then
+          print*,'delta source'
+          if(ndofn.eq.1)then
+            do ii=1,nodalSrc
+              if(ii.eq.2)Icurr(1) = -1.0*Icurr(1)
+              !print*,Icurr
+              A_F((srcLoc(ii)-1)*ndofn+1,1) = -Icurr(1)
+            end do
+          else
+            do ii=1,nodalSrc
+              if(ii.eq.2)Icurr(1) = -1.0*Icurr(1)
+              !print*,Icurr
+              A_F((srcLoc(ii)-1)*ndofn+1,1) = -Icurr(1)
+              A_F((srcLoc(ii)-1)*ndofn+2,1) = -Icurr(2)
+            end do
+          endif
+        end if
+      endif
+      
+      !----- Setting MKL-Solver Parammeters -----!
+      S_m     = size(A_K,2)  !antes ntotv
+      S_n     = size(A_K,2)  !antes ntotv
+      S_ldSol = max(1,S_n)
+      S_trans = 'N'
+      S_nrhs  = 1
+      
+      allocate( AK_LU(ldAKban,ntotv), u_sol(S_ldSol,1) ) 
+      allocate( S_ipiv(max(1,min(S_m, S_n)) ) )
+      
+      print*, ' '
+      print*, '!================ MKL Solver ==================!'
+      print*,'  •SETTING VARIABLES FOR MKL LIBRARY.....'
+      AK_LU = A_K     !AK_band(ldab,*) The array AK_band contains the matrix A_K in band storage
+      u_sol = A_F     !Sol_vec will be rewrited by LAPACK solution avoiding lose A_F
+      
+      !---------- Solving System of Equations by INTEL MKL solver -----------!
+      print*,'  •PERFORMING LU BAND DECOMPOSITION.....'
+      call dgbtrf(S_m, S_n, lowban, upban, AK_LU, ldAKban, S_ipiv, info)  
+      call MKLfactoResult('dgbtrf',info) 
+      print*,'  •SOLVING SYSTEM OF EQUATIONS.....'
+      
+      !---------- Computing nodal unknowns ux_i, uy_i and p_i ---------------!
+      call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AK_LU, ldAKban, S_ipiv, u_sol, S_ldSol, info )
+      call MKLsolverResult('dgbtrs',info)
    
-    ! call Res_Matlab(Ex_field)
-   
-    !---------- Memory Relase -----------!
-    deallocate( basfun, dN_dxi, dN_deta, BVs, nofix, ifpre, presc)
-    deallocate(hes_xixi, hes_xieta, hes_etaeta) 
-  
-  else !static case
-    
-    call ApplyBVs(nofix,ifpre,presc,A_K, A_F)
-    
-    !Applying the source term for DC simulation  j = I*δ(x-x0)δ(y-y0)
-    if(BCsProb.eq.5)then
-      continue
-    else
-      if(((ndofn.eq.1).or.(ndofn.eq.3)).and.((exacSol.eq.5).or.(exacSol.eq.2)))then
-        print*,'delta source'
-        if(ndofn.eq.1)then
-          do ii=1,nodalSrc
-            if(ii.eq.2)Icurr(1) = -1.0*Icurr(1)
-            !print*,Icurr
-            A_F((srcLoc(ii)-1)*ndofn+1,1) = -Icurr(1)
-          end do
-        else
-          do ii=1,nodalSrc
-            if(ii.eq.2)Icurr(1) = -1.0*Icurr(1)
-            !print*,Icurr
-            A_F((srcLoc(ii)-1)*ndofn+1,1) = -Icurr(1)
-            A_F((srcLoc(ii)-1)*ndofn+2,1) = -Icurr(2)
-          end do
-        endif
-      end if
-    endif
-      !A_F = 0.0
-    !---------- Memory Relase -----------!
-    allocate( AK_LU(ldAKban,ntotv), u_sol(S_ldSol,1)) 
-    allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
-    
-    !allocate( S_work(workdim), S_iwork(S_ldSol), S_ferr(S_nrhs), S_berr(S_nrhs) )
-    
-    print*, ' '
-    print*, '!================ MKL Solver ==================!'
-    print*,'  •SETTING VARIABLES FOR MKL LIBRARY.....'
-    AK_LU = A_K     !AK_band(ldab,*) The array AK_band contains the matrix A_K in band storage
-    u_sol = A_F     !Sol_vec will be rewrited by LAPACK solution avoiding lose A_F
-   
-    !---------- Solving System of Equations by INTEL MKL solver -----------!
-    print*,'  •PERFORMING LU BAND DECOMPOSITION.....'
-    call dgbtrf(S_m, S_n, lowban, upban, AK_LU, ldAKban, S_ipiv, info)  
-    call MKLfactoResult('dgbtrf',info) 
-    print*,'  •SOLVING SYSTEM OF EQUATIONS.....'
-    !---------- Computing nodal unknowns ux_i, uy_i and p_i ---------------!
-    call dgbtrs( S_trans, S_n, lowban, upban, S_nrhs, AK_LU, ldAKban, S_ipiv, u_sol, S_ldSol, info )
-    call MKLsolverResult('dgbtrs',info)
-   
-    !---------- Computing E=-∇u or -∂B/∂t=∇xE -----------------------------!
-    call PostPro_EMfield(basfun, dN_dxi, dN_deta, hes_xixi, hes_xieta, hes_etaeta, u_sol, grad_u_sol)
-    
-    !---------- Print and write results -----------------------------------!
-    call GID_results(u_sol, grad_u_sol) 
-    !call Res_Matlab(u_sol)
-    
-    !print*, ' '
-    !print*, 'Shape of Global K: ',shape(A_K)
-    !print*, 'Shape of Global F: ',shape(A_F)
-    !print*, 'Shape of Solution: ',shape(u_sol)
-    write(*,*)
-    !---------- Memory Relase ---------------------------------------------!
-    DEALLOCATE( A_F, A_K, AK_LU, u_sol)
-    deallocate( basfun, dN_dxi, dN_deta, BVs,  nofix, ifpre, presc)
-    deallocate(hes_xixi, hes_xieta, hes_etaeta) 
-    
-  endif
+      !---------- Computing E=-∇u or -∂B/∂t=∇xE -----------------------------!
+      call PostPro_EMfield(basfun,dN_dxi,dN_deta,hes_xixi,hes_xieta,hes_etaeta,u_sol,grad_u_sol)
+      
+      !---------- Print and write results -----------------------------------!
+      call GID_results(u_sol, grad_u_sol) 
+      !call Res_Matlab(u_sol)
+      
+      write(*,*)
+      !---------- Memory Relase ---------------------------------------------!
+      DEALLOCATE( A_F, A_K, AK_LU, u_sol)
+      deallocate( basfun, dN_dxi, dN_deta, BVs,  nofix, ifpre, presc)
+      deallocate(hes_xixi, hes_xieta, hes_etaeta) 
+      
+    case DEFAULT
+      PRINT*, ' ' 
+      print*, 'In Problem Type Transient or Static'
+      write(*,*) 'Invalid Problem Type.'
+      stop
+  end select
 
-  call cpu_time(finish)
-  write(*,'(A11,f15.5)   ')' CPU-Time =', start
-  write(*,'(A11,f15.5)   ')' CPU-Time =', finish
-  write(*,'(A11,f15.5,A8)')' CPU-Time =', (finish-start)/60.0, ' minutes', ' '
+  call cpu_time(finish); call fdate(date); 
+  write(*,'(A11,f14.5,A,f5.2,A,A)   ')' &
+    &CPU-Time =', finish,' ~',((finish-start)/60.0)/10.0,' minutes. Finished on: ', date
+  print*,' ' 
 
 end program main_CDR3d
 
