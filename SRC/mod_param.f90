7596b21e3833db49257adbc59c9207cf20f99e90
Marco Oliva <maoliva@cimne.upc.edu>
2024-01-31 13:39:24 +0100

    PARAM: Se hicieron grandes modificaciones debido al algoritmo de 2.5D, primero, para los archivos de postproceso .msh y .res se creo un vector de a lo sumo 14 elementos que contienen los nombres de estos archivos como _kXX donde XX indicara 01, 02, 03, 14 y los archivos de salida seran el nombre del input file mas este identificado con extension .post.res y .post.msh, de igual forma para los archivos que contienen la incognita transformada en la posicion del receptor indicado en el input file, se construiran archivos kyXX.dat, finalemnte los numeros de onda se construyen desde 1 y hasta 14, el nombre del archvio final tras la TIF se puede actualizar aqui aunque creo que PODRIA ACTUALIZARLO DIRECTAMENTE SIN TENER QUE PONERLO EN PARAMETERS, nuevos parametros de entrada por input file como la conductividad sigma y el operador diferencial que se esta resolviendo oper, las variables que contienen los nombres de los archivos fueron definidas con allocatable y se allocate tras leer el tot?ky por input file, se acomodo el frmato de lectura segun correspondia, finalmente se debe agregar read por cada seccion del input file para mejorar la trasabilidad de errores

--- SRC/mod_param.f90
+++ SRC/mod_param.f90
@@ -2,20 +2,21 @@ module param
   
   implicit none
 
-  character(len=12) :: File_Nodal_Vals, error_name, coord_name, conec_name, profile_name, mesh_file
-  character(len=12) :: files_ky(10), File_3DNodal_Vals
-  character(len=14) :: testID
   character(len=20) :: shape_spec_file
-  character(len=4)  :: ProbType, ElemType, initElemType
-  character(len=2)  :: refiType
-  character(len=2)  :: splits, TwoHalf
+  character(len=14) :: testID
+  character(len=12) :: File_Nodal_Vals, error_name, coord_name, conec_name, profile_name, mesh_file
+  character(len=12) :: File_3DNodal_Vals
+  character(len=8)  :: File_Nodal_Vals_ky
+  character(len=4)  :: ProbType, ElemType, initElemType, ky_id, oper
+  character(len=2)  :: refiType, splits, TwoHalf
   integer           :: nBVs, nBVscol, nband, t_steps, exacSol, BCsProb
   integer           :: upban, lowban, totban, ldAKban  !variables defined in GlobalSystem
   integer           :: DimPr, initnne, nne, ndofn, totGp, kstab, ktaum, maxband, theta, Src_ON
   integer           :: i_exp, nodalSrc, nodalRec, postpro, signal, srcType, srcRHS!, srcLoc
   integer           :: nelem, nnodes, nevab, ntotv, initnevab, initntotv,initNodes, initElem, tot_ky, idk_y
   real              :: hnatu, patau
-  double precision  :: Cu,lambda, ell, helem, n_val, time_ini, time_fin, delta_t, ky_min, ky_max, y_iFT,  k_y
+  double precision  :: Cu,lambda, ell, helem, n_val, time_ini, time_fin, delta_t
+  double precision  :: ky_min, ky_max, y_iFT,  k_y, sigma
   double precision, allocatable, dimension(:,:)     :: ngaus, weigp
 
   double precision, allocatable, dimension(:,:)     :: coord !, coordRef
@@ -24,6 +25,8 @@ module param
   double precision, allocatable, dimension(:,:,:)   :: conma
   double precision, allocatable, dimension(:,:)     :: reama !Tensor materials
   double precision, allocatable, dimension(:)       :: force, Icurr, WaveNumbers !Force and Current vector (respectively)
+  character(len=8), allocatable, dimension(:) :: files_ky        
+  character(len=4), allocatable, dimension(:) :: nodal_ky
   integer         , allocatable, dimension(:)       :: srcLoc, recLoc
 
   contains
@@ -48,7 +51,7 @@ module param
       open(5, file=fileplace//name_inputFile, status='old', action='read',IOSTAT=stat, IOMSG=msg)
       
       read(5, 100,iostat=stat,iomsg=msg) &
-      ProbType,DimPr,ndofn,totGp,exacSol, srcRHS, BCsProb, postpro,&
+      ProbType,DimPr,ndofn,totGp,exacSol, srcRHS, BCsProb, postpro, sigma, oper,&
       mesh_file, initnne, i_exp, hnatu, refiType,&
       kstab, ktaum, patau, n_val, helem, Cu, ell, lambda,&
       TwoHalf, ky_min, ky_max, tot_ky, splits, y_iFT, &
@@ -206,14 +209,28 @@ module param
         ! Asignar memoria para el vector
         allocate(WaveNumbers(tot_ky))
         ! Generar el vector logarítmico
-        do ii = 1, tot_ky !Esto lo tengo que arreglar para que no se modifique cuando divida el problema
+        
+        do ii = 1, tot_ky 
           WaveNumbers(ii) = 10.0**(log10(ky_min)+(log10(ky_max/ky_min)/real(tot_ky-1))*real(ii-1) )
         end do
-        ! WaveNumbers = (/1.0D-6, 5.0D-6, 5.0D-5, 5.0D-4, 3.0D-3, 1.0D-2, 2.5D-2, 9.0D-2, 2.5D-1, 5.0D-1/)
-        files_ky = (/'500H2lf_ky01','500H2lf_ky02','500H2lf_ky03','500H2lf_ky04','500H2lf_ky05',&
-        &           '500H2lf_ky06','500H2lf_ky07','500H2lf_ky08','500H2lf_ky09','500H2lf_ky10'/)
-        File_3DNodal_Vals = "3D_E_field05"
-        shape_spec_file = "shape_spectrum05.dat"
+        
+        ! WaveNumbers=(/1.0D-6, 5.0D-6, 1.0D-5, 5.5D-5, 5.0D-4,&
+        ! &             2.5D-3, 5.0D-3, 1.0D-2, 2.5D-2, 5.0D-2,&
+        ! &             8.0D-2, 1.0D-1, 2.5D-1, 5.0D-1/)
+        ! ky_min = WaveNumbers(1) 
+        ! ky_max = WaveNumbers(tot_ky)
+        
+        nodal_ky=(/'_k01','_k02','_k03','_k04','_k05','_k06','_k07',&
+        &          '_k08','_k09','_k10','_k11','_k12','_k13','_k14'/)
+        
+        files_ky = (/'ky01.dat','ky02.dat','ky03.dat','ky04.dat','ky05.dat',&
+        &            'ky06.dat','ky07.dat','ky08.dat','ky09.dat','ky10.dat',&
+        &            'ky11.dat','ky12.dat','ky13.dat','ky14.dat'/)
+
+        File_3DNodal_Vals = "3D_Potential"
+                           ! 3D_Potential
+        shape_spec_file = "DC_spectrum_correc3.dat"
+        File_Nodal_Vals_ky = File_Nodal_Vals
       else
         tot_ky = 1
         k_y    = 1.0
@@ -258,11 +275,11 @@ module param
       
       !Initial elemental and global variables, it will changes if refination is selected.
       
-      100 format(7/ ,11x, A4,/, 7(11x,I5,/),                              2/,&  !model parameters
+      100 format(7/ ,11x, A4,/, 7(11x,I5,/), 11x,F15.7,/, 11x,A4,/,       2/,&  !model parameters
       &          11x,A13,/, 2(11x,I7,/), 11x,F7.2,/, 11x,A2,/,            2/,&  !geometry
       &          2(11x,I5,/), 3(11x,F10.5,/), 3(11x,F15.5,/),             2/,&  !stabi
-      &          11x,A,/, 2(11x,e15.5,/), 11x,I3,/,11x,A,/, 11x,F15.5,/,  2/,&  !Fourier Transform
-      &          11x,I1,/, 2(11x,e15.7,/), 2(11x,I5,/),                   2/,&  !time
+      &          11x,A,/, 2(11x,e12.5,/), 11x,I3,/,11x,A,/, 11x,F10.3,/,  2/,&  !Fourier Transform
+      &          11x,I1,/, 2(11x,e12.7,/), 2(11x,I5,/),                   2/,&  !time
       &          11x,A14,/, 5(11x,A12,/),1/ )                                   !output files
      
       101 format(1/,F12.5,2/)
