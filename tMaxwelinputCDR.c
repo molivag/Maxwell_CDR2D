c45b186f12f1067182ec1ad7357e4af3ed4e5c06
Marco OG <mogeofisico@gmail.com>
2024-05-01 02:24:58 +0200

    INPUT: Se agrega el bloque de PHYSICAL_PROBLEM, DimPr, ndofn y Oper se eliminaron del archivo de entrada para facilitar la automatizacion del codigo, se eliminan los tensores del input file DIFMA, CONMA, REAMa, FORCE

--- tMaxwelinputCDR.c
+++ tMaxwelinputCDR.c
@@ -6,20 +6,30 @@
  
 # > > > > > > > Model Parameters
 ProbType = TIME           !Problem type TIME=transient, other=static
-DimPr    = 2              !Dimension del problema
-ndofn    = 3              !Degrees of freedom
 totGp    = 3              !1,4,9 for Q, 1,3,7 for P
 exacSol  = 3              !0=None;   1=SinglrSol  ; 2=FullSpace; 3=Algebraic; 4=Double Line
 srcRHS   = 0              !0=scalar; 1=SingularSol; 2=Maxwell_Polynom; 3=Lapalace_Polynom
-BCsProb  = 2              !1=Ldomain; 2=Maxwell; 3=MaxwellPoly; 4=Lapalace_Poly; 5=Cavity-Driven Flow 
+BCsProb  = 0              !1=Ldomain; 2=Maxwell; 3=MaxwellPoly; 4=Lapalace_Poly; 5=Cavity-Driven Flow 
 postpro  = 2              !Execution of post-processing routine 1=yes, 2=no 
 sigma    = 1.0            !Conductivity of the medium
-operator = MAXW           !PDE being solved LAPL=Laplacian; MAXW=curl-curl
+
+$********************************************************************************
+PHYSICAL_PROBLEM
+$----------------------------------------------------------------------------------
+  Maxwell_In_Non_Convex_Domain
+  Cavity_Driven_Flow
+  Direct_Current_Electrical_Resistivity_in_2.5-D
+  Electric_Field_Excited_By_A_Double_Line_Source
+ >Horizontal_Electric_Dipole_in_3-D_A_Wrong_capture_of_solution
+  Transient_Electromagnetic_in_2.5-D
+$----------------------------------------------------------------------------------
+END_PHYSICAL_PROBLEM
+$*********************************************************************************
 
 #***************Geometry
 #---------!File .msh that contains the mesh
-meshfile = TEM_VMD_ground_air.msh  
-view     = xy             !The 2D view x-y (distance) or x-z (depth)
+meshfile = TEM_HED_ground_air.msh  
+view     = xz             !The 2D view x-y (distance) or x-z (depth)
 nne      = 3              !Nodes per element Q:4-9; P:3-6
 i_exp    = 0              !Exponent of characteristic mesh size 3,4,5 or 6 2^(-i)
 hnatu    = 1.0            !Reference element length
@@ -39,109 +49,40 @@ ell      = 1000.0          !Constante de longitud
 TwoHalf  = N              !If it is dealing with a 2.5D modeling (Y) or not (N)
 ky_min   = 1.0e-3         !Smallest wave number
 ky_max   = 1.0e0          !Grater wave number
-tot_ky   = 4             !Total wave numbers
+tot_ky   = 3              !Total wave numbers
 splits   = N              !If the problem is splited then run as kind of parallel
 y_iFT    = 0.0            !Position at y-coordinate where the IFT will be computed.
 #------------!File to plot shape spectrum
-specFile = xxxxxxtransformed_Voltage.dat
+specFile = TEM_3D2D_test05_transformed field.dat
 #------------!File name after inverse Fourier Transform 
-File_iFT = Final 3D Direct Current Impresion_
+File_iFT = 3D TEM response
 
-# > > > > > > > Time Discretization
+# > > > > > > > Transient Parameters
 theta    = 2              !BDF1=2 ;CN=3; BDF2=4
-time_ini = 1.42000e-7     !Starting time simulation (simulation always starts at 0?)
+time_ini = 1.42000e-9     !Starting time simulation (simulation always starts at 0?)
 time_fin = 6.12800e-5     !Total time simulated in [s]  --> 1800 microseconds
-t_steps  = 10             !Number of time steps
-Src_ON   = 2              !Time at Source is turned ON
+t_steps  = 10              !Number of time steps
+Src_ON   = 4              !Time at Source is turned ON KIZA ESTO NO SE USE Y BASTA CON SIGNAL Y CAMBIO POR tw
+signal   = 1              !Source waveform: 1=step-on; 2=step-off; 3=triangular
+initCond = 0              !Initial Condition: 0=None; 1=Double-Line; 2=VMD
 
 # > > > > > > > Name outPut Files
-testID   = TEM_2point5D_test_04
-postpro  = TEM_2point5D_test_04
+testID   = No_Es_TEM_3D2D_HED_test_01
+postpro  = No_Es_TEM_3D2D_HED_test_01
 Error    = xxxxxxxx@xxx
 Cordina  = xxxxxxxxxxxx
 Conecti  = xxxxxxxxxxxx
-Profile  = xxxxxxxxxxxx
-
-# > > > > > > > Physical Properties
-#DIFMA_xx                  !Diffusion tensor		--     --
-1.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0	| Re_ex |
-0.0 , 1.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0	| Re_ey |
-0.0 , 0.0  , 1.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0	| Re_ez |
-0.0 , 0.0  , 0.0 , 1.0  , 0.0  , 0.0 , 0.0  , 0.0	| Re_p  |
-0.0 , 0.0  , 0.0 , 0.0  , 1.0  , 0.0 , 0.0  , 0.0	| Im_ex |
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 1.0 , 0.0  , 0.0	| Im_ey |
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 1.0  , 0.0	| Im_ez |
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 1.0	| Im_p  |
-#DIFMA_xz						--     --
-0.0 , -1.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-1.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-#DIFMA_zx
-0.0 , 1.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
--1.0 , 0.0 , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-#DIFMA_zz
-1.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 1.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 1.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-0.0 , 0.0  , 0.0 , 0.0  , 0.0  , 0.0 , 0.0  , 0.0
-#CONMAT_x                   !Convection tensor
-0.0 , 0.0 , 1.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-1.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-#CONMAT_z
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 1.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 1.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-#REAMA                     !Reaction tensor
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0 , 0.0
-#FORCE                     !Force tensor
-0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+Profile  = TEM__HED__testFile
 
 # > > > > > > > Source Configuration
-#Icurr
-1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0
+#electric Current Vectror
+8.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0
 
-#Nodal source Location
-nodalSrc = 2            !Number of nodes will contain the source
-19
+#Location
+nodalSrc = 1            !Number of nodes will contain the source
 13
 
-#Time waveform
-signal   = 1            !Signal in time: 1=step-on; 2=step-off; 3=triangular
-
-# > > > > > > > Receiver Locations
+#Receivers
 nodalRec = 1            !Number of nodes as a receiver
-0.0  , 0.0
+500.0 ,0.0, 0.0
 
