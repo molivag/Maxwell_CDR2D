fdc799c9bd0a302834e100df564f06bc61753f29
Marco OG <mogeofisico@gmail.com>
2024-05-03 15:03:57 +0200

    TIME: La rutina Initial_Condition se separo la forma de la señal de tiempo y esta señal se paso a la nueva subrutina ShapeTimeSignal, en la rutina Initial_Condition se definiron hasta ahora 3 casos de condicion inicial, 0 para no initialCond use source term, 1 para double line y 2 para VMD de TEM 2.5D pero aun no la he codificado (qiza ni la use), en shape se definen los mismos caso de triangular, step-on y step-off se mantiene la de step-on in whole simulation, en TimeIntegration, antes de inicializar u_pre se llaman las subrutinas Initial_Condition y ShapeTimeSignal, en todas las llamadas a GID_PostProcess se agrego la variable de entrada i_WaveNumber para identificar que leyenda se imprimira en pantalla conforme se vaya ejecutando el codigo. Se coloco un if dentro de timesteping que dependiendo de si se usa un termino de fuente Jsource o una condicion inicial o ninguno de los dos, el RHS sea uno u ootro con la finalidad de reducir memoria

--- SRC/mod_timeInt.f90
+++ SRC/mod_timeInt.f90
@@ -1,13 +1,17 @@
 module timeInt
   use param
+  use tensor_inputs 
   use library
-  use E0field
+  ! use E0field
   use sourceTerm
 
   contains
 
-    subroutine initialCondition(presc,ifpre, nofix, shapeTime, Uinit) 
-      
+    ! subroutine initialCondition(presc,ifpre, nofix, shapeTime, Uinit) 
+    subroutine Initial_Condition(i_WaveNum, presc,ifpre, nofix,  E0) 
+              ! Efield_WholeSpace(t,t0, E0) !---> como t0 se da una sola vez, entonces se quita de variable de entrada
+              ! Efield_WholeSpace(t, E0)
+              
       !* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
       ! Routine to proyect the initial condition into the FE space:
       ! 
@@ -26,39 +30,175 @@ module timeInt
       implicit none
      
       !external                                      :: dgbtrf, dgbtrs, dgbrfs
+      double precision,              dimension(ndofn,nBVs) ,intent(in) :: presc
+      integer,                       dimension(ndofn,nBVs) ,intent(in) :: ifpre
+      integer,                       dimension(nBVs)       ,intent(in) :: nofix
+      integer,                                              intent(in) :: i_WaveNum
+      
+      ! double precision, allocatable, dimension(:,:)                    :: dummy
+      ! integer                                                   :: 
+      double precision                                          :: t0
+      ! double precision, dimension(ntotv)                        :: E0
+
+      
+      ! character(len=*), parameter           :: fileplace3 = "Exact_Sol_TEM/2D_DoubleLine_WholeSpace/"
+      double precision, parameter                                      :: pi = 4*atan(1.d0)
+      ! double precision, intent(in)          :: tEz             !correspond to nt 
+      ! integer         , intent(in)          :: time            !correspond to time 
+      double precision                                                 :: sigma, S, x1,y1,x2,y2,x,y
+      double precision                                                 :: Curr_x,Curr_y, angle_loop
+      double precision                                                 :: facto, theta2, rho1, rho2, exp1, exp2
+      double precision                                                 :: Ez_r_x, Ez_r_y
+      integer                                                          :: itest, inode, ii, ipoin, itotv
+      double precision                                                 :: Mr,Mx,My, x_profile, mu
+      double precision, dimension(1,nnodes)                            :: xcor, ycor
+      ! double precision, intent(out)                                    :: Ez_r(ntotv)
+      double precision, intent(out)                                    :: E0(ntotv,1)
      
-      double precision, dimension(ndofn,nBVs)       ,intent(in) :: presc
-      integer, dimension(ndofn,nBVs)                ,intent(in) :: ifpre
-      integer, dimension(nBVs)                      ,intent(in) :: nofix
-      double precision, allocatable, dimension(:,:)             :: dummy
-      double precision, dimension(t_steps)                      :: u
-      integer                                                   :: tw, t, itotv
-      double precision                                          :: tEz
-      double precision, dimension(ntotv)                        :: E0
-      double precision, dimension(ntotv,1)         ,intent(out) :: Uinit
-      double precision, dimension(t_steps)         ,intent(out) :: shapeTime
-      !double precision                             ,intent(out):: delta_t
       
-      allocate( dummy(ldAKban,ntotv) )
       
-      u  = 1.0
-      tw = 1 !time*width how strong the impulse is
-      Uinit = 0
-      tEz = time_ini
-      t = 0
+      t0 = time_ini !este es el tiempo inicial dado en input file
+      ! t = 0
       
-      ! call Efield_WholeSpace(t,tEz, E0)
+      xcor  = spread(coord(1,:),dim = 1, ncopies= 1)
+      ycor  = spread(coord(2,:),dim = 1, ncopies= 1)
+      
+      select case(initCond)
+        case(0)
+          if(i_WaveNum ==0 .or. i_WaveNum==1)then
+            ! print*, '* * *'
+            print*, '- No initial Condition defined, source term gonna be used'
+          endif
+          continue
+          
+        case(1) !Double Line:  eq. (17) of paper by Zhang & Liu (2021)
+          if(i_WaveNum ==0 .or. i_WaveNum==1)then
+            ! print*, '* * *'
+            print*, '- Initial condition for the Double Line problem'
+          endif
+          
+          E0 = 0.0 
+          mu = 1.0/lambda
+          angle_loop = 90.0
+          sigma = 0.01
+          x1 = coord(1,Srcloc(1))
+          y1 = coord(2,Srcloc(1))
+          x2 = coord(1,Srcloc(2))
+          y2 = coord(2,Srcloc(2))
+          S  = abs(x1*x2) ! not needed, cancels out
+          
+          angle_loop=angle_loop*pi/180. ! theta=-30 deg
+          Mr=Icurr(1)*S
+          Mx=Mr*sin(angle_loop)
+          My=Mr*cos(angle_loop)
+          Curr_x= Mx/S
+          Curr_y= My/S
+          
+          ! loop coord.
+          facto =4.0*pi*t0
+          theta2=mu*sigma/(4.*t0) ! = theta^2
+          
+          do inode = 1, nnodes  !simple Function
+            x = coord(1,inode)
+            y = coord(2,inode)
+            
+            rho1 = sqrt( (x-x1)**2 + (y-y1)**2 )
+            rho2 = sqrt( (x-x2)**2 + (y-y2)**2 )
+            exp1 = exp(-rho1*rho1*theta2)
+            exp2 = exp(-rho2*rho2*theta2)
+            ! Source x
+            Ez_r_x = -mu*(Curr_x+Curr_y)/facto*exp1
+            Ez_r_y = +mu*(Curr_x+Curr_y)/facto*exp2
+
+            !Electric Field
+            ! Ez_r(inode) = Ez_r_x + Ez_r_y
+            E0(inode,1) = Ez_r_x + Ez_r_y
+            ! if(ndofn.eq.1)then
+            !   Ez_r(inode) = Ez_r_x + Ez_r_y
+            ! else
+            !   Ez_r(ndofn*inode-2) = Ez_r_x + Ez_r_y
+            ! endif
+           
+          end do
+        
+        case(2) !VMD  eq. (125) of paper by Li & Cheng (2023)
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          
+          case default
+            write(*,*) ' No initial condition defined'
+            stop
+        end select
+      
+      ! call Efield_WholeSpace(t,t0, E0)
       ! do itotv=1,ntotv
-      !   Uinit(itotv,1) = E0(itotv)
+        ! Uinit(itotv,1) = E0(itotv)
       ! enddo
-      ! ! call ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
+      ! call ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
       
-      !--Select a signal shape function in time
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      !open(unit=5, file= fileplace3//"E0exact.dat", status='old', action='read')
+      !write(5,1)x,Ez_r_x,Ez_r_y,Ez_r
+      918 format(I7,3x,E14.6,3x,E14.6) !format for res velocity
+      906 format(I7,2(3x,f9.4)) !format for msh
+      
+      
+      
+      
+      
+      return
+      stop 
+    end subroutine Initial_Condition
+    !
+    != = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
+    !
+    subroutine ShapeTimeSignal(i_WaveNum, shapeTime)
+      !si esto es una sola variable de salida, puede quedar como una funcion
+      
+      implicit none
+      integer, intent(in)                               ::  i_WaveNum
+      integer                                           :: tw, t
+      double precision, dimension(t_steps)              :: u
+      double precision, dimension(t_steps), intent(out) :: shapeTime
+      
+      u  = 0.0
+      tw = 1 !time*width how strong the impulse is must be grather or equal to t_steps
+      ! t  = 0
+      
       select case(signal)
-        case(1) !step-on
+        case(1) !step-on (for test is a continuous turned on signal)
+          if(i_WaveNum ==0 .or. i_WaveNum==1)then
+            print*, '- Step-on source waveform turned on till end of simulation'
+            print*, '* * *'
+          endif
+          
+          ! With the step-on signal, a source current is turned on with a step-function
+          ! and left on (source current remains J_p = 1). The initial E fields start from E=0 everywhere.
+          ! The fields then converge towards the DC value over time.
+          
           do t = 1, t_steps
-            if(t.le.tw)then
-              u(t) = 0.0
+            if(t.ge.tw)then
+              u(t) = 1.0
             !elseif(t.gt.10*tw)then!.and.t.le.2*tw)then
             !  u(t) = 0.0
             !elseif(t.gt.2*tw)then
@@ -66,6 +206,10 @@ module timeInt
             endif
           end do
         case(2) !step off
+          if(i_WaveNum ==0 .or. i_WaveNum==1)then
+            print*, '- Step-off source waveform selected'
+            print*, ' '
+          endif
           do t = 1, t_steps
             if(t.le.tw)then
               u(t) = 1.0
@@ -76,33 +220,27 @@ module timeInt
             endif
           end do
         case(3) !triangular
+          if(i_WaveNum ==0 .or. i_WaveNum==1)then
+            print*, '* * *'
+            print*, '- Triangular source waveform selected'
+            print*, ' '
+          endif
           do t = 1, t_steps
             if(t.le.tw)then
               u(t) = 1.0/tw * t
             elseif(tw.lt.t.and.t.lt.2*tw)then
               u(t) = 1. - 1./tw * (t-tw)
-            !else
-            !  u(t) = 0.0
+            else
+             u(t) = 0.0
             endif
           end do
         case default
-          write(*,*) 'No function in time defined'
+          write(*,*) 'Signal shape in time not defined'
           stop
       end select
-      
       shapeTime = u 
       
-      
-      !do t =1,t_steps
-      !  print"(I0, 1x, f5.3)", t, shapeTime(t)
-      !end do
-      
-      
-      
-      deallocate(dummy)
-      return
-      stop 
-    end subroutine initialCondition
+    end subroutine ShapeTimeSignal
     !
     != = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =
     !
@@ -126,9 +264,10 @@ module timeInt
       double precision, allocatable, dimension(:,:)       :: LHS!, lhs_BDF2
       double precision, allocatable, dimension(:,:)       :: u_pre, u_curr, u_fut, Mu_pre
       ! double precision, allocatable, dimension(:)         :: S_ferr, S_berr, S_work
-      double precision, allocatable, dimension(:,:)       :: Jsource, Jsource_pre
+      double precision, allocatable, dimension(:,:)       :: Jsource, Jsource_pre, dummy
       double precision             , dimension(ntotv,1)   :: RHS, u_init!, F_plus_MU, rhs_BDF2
-      double precision             , dimension(t_steps+1) :: shapeTime
+      ! double precision             , dimension(t_steps+1) :: shapeTime
+      double precision             , dimension(t_steps) :: shapeTime
       double precision, allocatable, dimension(:,:)       :: store_Spec
       integer         , allocatable, dimension(:)         :: S_ipiv!, S_iwork
       double precision                                    :: nt, ttt
@@ -137,12 +276,14 @@ module timeInt
       double precision, allocatable,dimension(:),intent(out) :: Ex_field
       
       
+      !* * * Aqui se puede hacer un call a una subroutina que reduzca estas lineas * * *!
       if(TwoHalf == 'Y')then !Just if it is dealing with a 2.5D problem, these variable gonna be updating 
         ky_id = nodal_ky(i_WaveNum)
         k_y = WaveNumbers(i_WaveNum) !Value of wave number for current problem (used in reama)
       else
         continue
       endif
+      !* * * Aqui se puede hacer un call a una subroutina que reduzca estas lineas * * *!
       
       allocate( LHS(ldAKban,ntotv))
       ! allocate(lhs_BDF2(ldAKban,ntotv))
@@ -157,18 +298,27 @@ module timeInt
       S_trans = 'N'
       S_nrhs  = 1
       
+      allocate( dummy(ldAKban,ntotv) ) ! esto es Global K para poder llamar a ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
       allocate( u_pre(S_ldSol, 1), u_fut(S_ldSol, 1) )
       allocate( S_ipiv(max(1,min(S_m, S_n)) ))  !size (min(m,n))
       allocate( Ex_field(t_steps+1))
       
-      u_curr = 0.0; u_pre  = 0.0; u_fut  = 0.0
-      Mu_pre = 0.0; RHS    = 0.0; LHS    = 0.0
-      time   = 0  ; nt = time_ini;ttt    = 0.0
+      u_curr = 0.0; u_pre  = 0.0  ; u_fut  = 0.0
+      Mu_pre = 0.0; RHS    = 0.0  ; LHS    = 0.0
+      time   = 0  ; nt = time_ini ; ttt    = 0.0
       !delta_t 1e-3!( time_fin - time_ini ) / (t_steps + 1.0)   !Step size
       
-      call initialCondition(presc,ifpre, nofix, shapeTime, u_init)
-      u_pre  = u_init                                   !u in present time 
+      call Initial_Condition(i_WaveNum, presc,ifpre, nofix, u_init)
+      call ShapeTimeSignal(i_WaveNum,shapeTime)
+      !Aqui deberia aplicar las condiciones de frontera para el U-inicial?
+      ! call ApplyBVs(nofix,ifpre,presc,dummy,Uinit)
+      deallocate(dummy)
+      u_pre  = u_init                                          !u in present time 
       
+      
+      
+      
+      !* * * Aqui se puede hacer un call a una subroutina que reduzca estas lineas * * *!
       !If it is dealing with a 2.5D problem
       if(TwoHalf == 'Y')then
         deallocate(A_F)
@@ -178,24 +328,35 @@ module timeInt
         end do
         call storeSpectrum(i_WaveNum, store_Spec)
       endif
+      !* * * Aqui se puede hacer un call a una subroutina que reduzca estas lineas * * *!
+      
       !write(*,*) ' '
-      call GID_PostProcess(1,u_pre, 'msh'    , time, nt, time_fin, Ex_field)
-      call GID_PostProcess(1,u_pre, 'res'    , time, nt, time_fin, Ex_field)
-      call GID_PostProcess(1,u_pre, 'profile', time, nt, time_fin, Ex_field)
+      
+      call GID_PostProcess(i_WaveNum, 1,u_pre, 'msh'    , time, nt, time_fin, Ex_field)
+      call GID_PostProcess(i_WaveNum, 1,u_pre, 'res'    , time, nt, time_fin, Ex_field)
+      call GID_PostProcess(i_WaveNum, 1,u_pre, 'profile', time, nt, time_fin, Ex_field)
       ! call storeSpectrum('TIME',u_fut, time)
       write(*,*) ' '
-      if(i_WaveNum ==1)print*, 'Starting time integration for all wavenumbers. . . . .'
+      if(i_WaveNum == 0)then
+        print*, '- Progress. . . . .'
+      else
+        if(i_WaveNum ==1) print*, 'Starting time integration for all wavenumbers. . . . .'
+      endif
       ! print 100,' time step:',time,'  = ',time_ini,' is the value of u by the initial condiction'
      
       select case(theta)
         !-------- 1st-order Backward Difference 
         case(2) 
           !Time-stepping
-          if(i_WaveNum ==1) then
-            write(*,*)'        < < < BDF1 Selected > > >'
-            write(*,*) ' - '
+          if(i_WaveNum == 0)then
+            continue
+          else
+            if(i_WaveNum ==1) then
+              write(*,*)'        < < < BDF1 Selected > > >'
+              write(*,*) ' - '
+            endif
+            print'(A34,I0,A3,E11.4)', " !-------> Time Integration for ky",i_WaveNum,"=",k_y
           endif
-          print'(A34,I0,A3,E11.4)', " !-------> Time Integration for ky",i_WaveNum,"=",k_y
           !do while(ttt < time_fin)
           time_stepping: do time = 1, t_steps
             nt = nt + delta_t!,time_fin,delta_t
@@ -203,10 +364,14 @@ module timeInt
             
             call prevTime(basfun,dN_dxi,dN_deta,hes_xixi,hes_xieta,hes_etaeta,S_ldsol,u_pre,Mu_pre)
             LHS  = (A_C + delta_t*A_K)
-            call currDensity(Jsource,time,shapeTime(time)) 
+            call currDensity(i_WaveNum, Jsource,time,shapeTime(time)) 
             if(oper == 'MAXW')then
-              if(TwoHalf=='Y'.or.TwoHalf=='N')then
+              if( (TwoHalf=='Y'.or.TwoHalf=='N') .and. initCond.eq.0)then
+                !si se trata de un problema 2.5D sin condicion inicial, entonces usa esto:'
                 RHS = (Mu_pre - delta_t*Jsource)
+              elseif((TwoHalf=='Y'.or.TwoHalf=='N') .and. initCond.ne.0)then
+                !si se trata de un problema 2.5D con condicion inicial, entonces Jsource=0 y se usa esto:'
+                RHS = (Mu_pre )
               endif
             else
               RHS = (delta_t*A_F + Mu_pre)
@@ -226,8 +391,8 @@ module timeInt
             !---------- Printing and writing results -----------!
             call infoTime(time)
             ! print'(I0, 1x, e15.5)',time, nt
-            call GID_PostProcess(1,u_fut, 'res'    , time, nt, time_fin, Ex_field)
-            call GID_PostProcess(1, u_fut, 'profile', time, nt, time_fin, Ex_field)
+            call GID_PostProcess(i_WaveNum,1,u_fut, 'res'    , time, nt, time_fin, Ex_field)
+            call GID_PostProcess(i_WaveNum,1, u_fut, 'profile', time, nt, time_fin, Ex_field)
             ! call GID_PostProcess(1, u_fut, 'spatial', time, nt, time_fin, Ex_field)
             
             !---------- Updating Variables ---------------------! 
@@ -360,8 +525,12 @@ module timeInt
            write(*,*) 'Not time integration method definded'
            stop
       end select
-      print'(A11,I0)',' !--> End ky',i_WaveNum
-      ! print*,' '
+      if(i_WaveNum == 0)then
+        continue
+      else
+        print'(A,I0)','-----------------------------------------------------------------> End ky',i_WaveNum
+        ! print*,' '
+      endif
       
       
       !print*, 'Shape of Global K: ',shape(LHS)
